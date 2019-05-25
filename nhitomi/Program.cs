31b89696a87073782bf83ed664c9b77cc8ef6ffc
phosphene47 <phosphene47@chiya.dev>
2019-05-26 04:49:20 +1000

    Restructure startup to use the generic host builder

--- nhitomi/Program.cs
+++ nhitomi/Program.cs
@@ -1,20 +1,54 @@
-// Copyright (c) 2018-2019 chiya.dev
-// 
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using nhitomi.Core;
+using nhitomi.Database;
+using nhitomi.Services;
+using Newtonsoft.Json;
 
 namespace nhitomi
 {
     public static class Program
     {
         static Task Main(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build()
-                .RunAsync();
+            new HostBuilder()
+                .ConfigureAppConfiguration((host, config) =>
+                {
+                    config
+                        .SetBasePath(host.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", false)
+                        .AddJsonFile($"appsettings.{host.HostingEnvironment.EnvironmentName}.json", true)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureServices((host, services) =>
+                {
+                    services
+                        // configuration
+                        .Configure<AppSettings>(host.Configuration)
+
+                        // logging
+                        .AddLogging(l => l
+                            .AddConfiguration(host.Configuration.GetSection("logging"))
+                            .AddConsole()
+                            .AddDebug())
+
+                        // background services
+                        .AddSingleton<DiscordService>()
+                        .AddHostedService<StatusUpdater>()
+                        .AddHostedService<FeedUpdater>()
+                        .AddSingleton<InteractiveManager>()
+                        .AddHostedService<ProxyListBroadcastService>()
+                        .AddSingleton<MessageFormatter>()
+
+                        // database
+                        .AddSingleton<IDatabase, DynamoDbDatabase>()
+
+                        // other stuff
+                        .AddHttpClient()
+                        .AddTransient(s => JsonSerializer.Create(new nhitomiSerializerSettings()));
+                })
+                .RunConsoleAsync();
     }
 }
