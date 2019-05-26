6fd9630651589500793a8859e45c25960f7052df
phosphene47 <phosphene47@chiya.dev>
2019-05-26 22:38:40 +1000

    Add ClientRegistry

---
+++ nhitomi.Core/Clients/ClientRegistry.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace nhitomi.Core.Clients
+{
+    public static class ClientRegistry
+    {
+        static readonly IReadOnlyDictionary<string, Type> _clientFactory = typeof(IDoujinClient).Assembly
+            .GetTypes()
+            .Where(t => !t.IsAbstract && t.IsClass && typeof(IDoujinClient).IsAssignableFrom(t))
+            .ToDictionary(t => t.Name.SubstringFromEnd("Client".Length), t => t);
+
+        public static string FixSource(string source)
+        {
+            foreach (var clientName in _clientFactory.Keys)
+                if (source.Equals(clientName, StringComparison.OrdinalIgnoreCase))
+                    return clientName;
+
+            return source;
+        }
+
+        // fixes trailing zeros if ID is an integer
+        public static string FixId(string id) => long.TryParse(id, out var longId) ? longId.ToString() : id;
+    }
+}
