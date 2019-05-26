9cd226c419dcc2febaa35926b6fb410c0481d88c
phosphene47 <phosphene47@chiya.dev>
2019-05-26 16:48:41 +1000

    Move DI initialization to Startup

--- nhitomi/Program.cs
+++ nhitomi/Program.cs
@@ -1,63 +1,14 @@
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using nhitomi.Core;
-using nhitomi.Database;
-using nhitomi.Services;
-using Newtonsoft.Json;
 
 namespace nhitomi
 {
     public static class Program
     {
-        static Task Main(string[] args) =>
+        static Task Main() =>
             new HostBuilder()
-                .ConfigureAppConfiguration((host, config) =>
-                {
-                    config
-                        .SetBasePath(host.HostingEnvironment.ContentRootPath)
-                        .AddJsonFile("appsettings.json", false)
-                        .AddJsonFile($"appsettings.{host.HostingEnvironment.EnvironmentName}.json", true)
-                        .AddEnvironmentVariables();
-                })
-                .ConfigureServices((host, services) =>
-                {
-                    var settings = host.Configuration.Get<AppSettings>();
-
-                    services
-                        // configuration
-                        .Configure<AppSettings>(host.Configuration);
-
-                    // logging
-                    services
-                        .AddLogging(l => l
-                            .AddConfiguration(host.Configuration.GetSection("logging"))
-                            .AddConsole()
-                            .AddDebug());
-
-                    // background services
-                    services
-                        .AddSingleton<DiscordService>()
-                        .AddHostedService<StatusUpdater>()
-                        .AddHostedService<FeedUpdater>()
-                        .AddSingleton<InteractiveManager>()
-                        .AddSingleton<MessageFormatter>();
-
-                    // database
-                    services
-                        .AddSingleton<IDatabase, DynamoDbDatabase>();
-
-                    // other stuff
-                    services
-                        .AddHttpClient()
-                        .AddTransient(s => JsonSerializer.Create(new nhitomiSerializerSettings()));
-
-                    // http server
-                    if (settings.Http.EnableProxy)
-                        services.AddHostedService<ProxyService>();
-                })
+                .ConfigureAppConfiguration(Startup.Configure)
+                .ConfigureServices(Startup.ConfigureServices)
                 .RunConsoleAsync();
     }
 }
--- nhitomi/Startup.cs
+++ nhitomi/Startup.cs
@@ -1,2 +1,59 @@
-// nhitomi uses the Generic Host of ASP.NET Core
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using nhitomi.Core;
+using nhitomi.Database;
+using nhitomi.Http;
+using nhitomi.Services;
+using Newtonsoft.Json;
 
+namespace nhitomi
+{
+    public static class Startup
+    {
+        public static void Configure(HostBuilderContext host, IConfigurationBuilder config)
+        {
+            config
+                .SetBasePath(host.HostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", false)
+                .AddJsonFile($"appsettings.{host.HostingEnvironment.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            // configuration
+            services
+                .Configure<AppSettings>(host.Configuration);
+
+            // logging
+            services
+                .AddLogging(l => l.AddConfiguration(host.Configuration.GetSection("logging"))
+                    .AddConsole()
+                    .AddDebug());
+
+            // discord services
+            services
+                .AddSingleton<DiscordService>()
+                .AddSingleton<InteractiveManager>()
+                .AddSingleton<MessageFormatter>()
+                .AddHostedService<StatusUpdater>()
+                .AddHostedService<FeedUpdater>();
+
+            // database
+            services
+                .AddSingleton<IDatabase, DynamoDbDatabase>();
+
+            // other stuff
+            services
+                .AddHttpClient()
+                .AddTransient(s => JsonSerializer.Create(new nhitomiSerializerSettings()));
+
+            // http server
+            services
+                .AddHostedService<HttpService>()
+                .AddSingleton<ProxyService>();
+        }
+    }
+}
