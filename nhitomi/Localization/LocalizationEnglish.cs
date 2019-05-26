3df517b6eddd89295204503bdc3b898176f81a44
phosphene47 <phosphene47@chiya.dev>
2019-05-26 23:57:04 +1000

    Fix english localization class

--- nhitomi/Localization/LocalizationEnglish.cs
+++ nhitomi/Localization/LocalizationEnglish.cs
@@ -5,5 +5,21 @@ namespace nhitomi.Localization
     public class LocalizationEnglish : Localization
     {
         public override CultureInfo Culture => new CultureInfo("en");
+
+        public override DoujinMessageLocalization DoujinMessage { get; } = new DoujinMessageEnglish();
+        public override DownloadMessageLocalization DownloadMessage { get; } = new DownloadMessageEnglish();
+        public override HelpMessageLocalization HelpMessage { get; } = new HelpMessageEnglish();
+
+        class DoujinMessageEnglish : DoujinMessageLocalization
+        {
+        }
+
+        class DownloadMessageEnglish : DownloadMessageLocalization
+        {
+        }
+
+        class HelpMessageEnglish : HelpMessageLocalization
+        {
+        }
     }
 }
