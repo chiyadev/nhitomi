735214eb1d49f229876820c783396837fad0cee1
phosphene47 <phosphene47@chiya.dev>
2019-05-26 23:11:12 +1000

    Fix doujin list interactive not showing attaching reaction triggers

--- nhitomi/Interactivity/DoujinListMessage.cs
+++ nhitomi/Interactivity/DoujinListMessage.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Discord;
 using nhitomi.Core;
+using nhitomi.Interactivity.Triggers;
 
 namespace nhitomi.Interactivity
 {
@@ -7,6 +9,17 @@ namespace nhitomi.Interactivity
     {
         public DoujinListMessage(EnumerableBrowser<Doujin> enumerable) : base(enumerable)
         {
+        }
+
+        protected override IEnumerable<ReactionTrigger> CreateTriggers()
+        {
+            yield return new FavoriteTrigger();
+            yield return new DownloadTrigger();
+
+            foreach (var trigger in base.CreateTriggers())
+                yield return trigger;
+
+            yield return new DeleteTrigger();
         }
 
         protected override Embed CreateEmbed(Doujin value) => DoujinMessage.CreateEmbed(value);
