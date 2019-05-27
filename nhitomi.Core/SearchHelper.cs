1cc40c5bff479de7813ca4ec766fe14de9ca4323
phosphene47 <phosphene47@chiya.dev>
2019-05-27 23:02:45 +1000

    Take IDatabase instead of DbContext

--- nhitomi.Core/SearchHelper.cs
+++ nhitomi.Core/SearchHelper.cs
@@ -6,15 +6,18 @@ namespace nhitomi.Core
 {
     public static class SearchHelper
     {
-        public static IQueryable<T> FullTextSearch<T>(this IQueryable<T> queryable, string query, DbContext db)
+        public static IQueryable<T> FullTextSearch<T>(this IQueryable<T> queryable, string query, IDatabase db)
         {
-            switch (db.Database.ProviderName)
+            if (!(db is DbContext context))
+                throw new ArgumentException($"{nameof(db)} is not a {nameof(DbContext)}.");
+
+            switch (context.Database.ProviderName)
             {
                 case "Pomelo.EntityFrameworkCore.MySql": return queryable.MySqlMatch(query);
                 case "Microsoft.EntityFrameworkCore.Sqlite": return queryable.SqliteMatch(query);
             }
 
-            throw new NotSupportedException($"Unknown database provider {db.Database.ProviderName}.");
+            throw new NotSupportedException($"Unknown database provider {context.Database.ProviderName}.");
         }
 
         //todo:
