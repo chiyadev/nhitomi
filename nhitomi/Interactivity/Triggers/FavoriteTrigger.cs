c90222b0d2cf0165fae012fd416ea7164b48e279
phosphene47 <phosphene47@chiya.dev>
2019-05-28 01:17:46 +1000

    Fix favorite trigger

--- nhitomi/Interactivity/Triggers/FavoriteTrigger.cs
+++ nhitomi/Interactivity/Triggers/FavoriteTrigger.cs
@@ -4,17 +4,23 @@ using System.Threading.Tasks;
 using Discord;
 using Microsoft.Extensions.DependencyInjection;
 using nhitomi.Core;
+using nhitomi.Discord;
 
 namespace nhitomi.Interactivity.Triggers
 {
     public class FavoriteTrigger : ReactionTrigger<IDoujinInteractive>
     {
+        const string _collectionName = "Favorites";
+
         public override string Name => "Favorite";
         public override IEmote Emote => new Emoji("\u2764");
         public override bool CanRunStateless => true;
 
         public override async Task RunAsync(IServiceProvider services, CancellationToken cancellationToken = default)
         {
+            var discord = services.GetRequiredService<DiscordService>();
+            var database = services.GetRequiredService<IDatabase>();
+
             // retrieve doujin
             var doujin = Interactive?.Doujin;
 
@@ -31,11 +37,36 @@ namespace nhitomi.Interactivity.Triggers
                     return;
             }
 
-            var channel = await _discord.Socket.GetUser(reaction.UserId).GetOrCreateDMChannelAsync();
+            do
+            {
+                var collection = await database.GetCollectionAsync(Context.User.Id, _collectionName, cancellationToken);
 
-            // add to or remove from favorites
-            if (await _database.TryAddToCollectionAsync(reaction.UserId, _favoritesCollection, doujin))
-                await channel.SendMessageAsync(_formatter.AddedToCollection(_favoritesCollection, doujin));
+                if (collection == null)
+                {
+                    // create new collection for favorites
+                    collection = new Collection
+                    {
+                        Name = _collectionName,
+                        Owner = new User
+                        {
+                            Id = Context.User.Id
+                        }
+                    };
+
+                    database.Add(collection);
+                }
+
+                // add to favorites collection
+                collection.Doujins.Add(new DoujinCollection
+                {
+                    DoujinId = doujin.Id
+                });
+            }
+            while (!await database.SaveAsync(cancellationToken));
+
+            var channel = await discord.Socket.GetUser(Context.User.Id).GetOrCreateDMChannelAsync();
+
+            //await channel.SendMessageAsync(_formatter.AddedToCollection(_favoritesCollection, doujin));
         }
     }
 }
