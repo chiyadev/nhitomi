9bb6260d0e24aad5bd15ddc31a3c52be6f9ce1c8
phosphene47 <phosphene47@chiya.dev>
2019-05-27 20:35:33 +1000

    Remove DynamoDB stuff

--- nhitomi/Database/UserInfo.cs
+++
@@ -1,9 +0,0 @@
-using Amazon.DynamoDBv2.DataModel;
-
-namespace nhitomi.Database
-{
-    public class UserInfo
-    {
-        [DynamoDBHashKey("userId")] public ulong UserId { get; set; }
-    }
-}
