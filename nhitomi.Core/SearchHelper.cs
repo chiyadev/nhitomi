f6b93b882c0607e1219a2663078438d3dd8887c0
phosphene47 <phosphene47@chiya.dev>
2019-05-27 23:04:06 +1000

    Fix full text helper not taking path to property

--- nhitomi.Core/SearchHelper.cs
+++ nhitomi.Core/SearchHelper.cs
@@ -1,27 +1,34 @@
 using System;
 using System.Linq;
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 
 namespace nhitomi.Core
 {
     public static class SearchHelper
     {
-        public static IQueryable<T> FullTextSearch<T>(this IQueryable<T> queryable, string query, IDatabase db)
+        public static IQueryable<T> FullTextSearch<T, TProperty>(this IQueryable<T> queryable, IDatabase db,
+            Expression<Func<T, TProperty>> path, string query)
         {
             if (!(db is DbContext context))
                 throw new ArgumentException($"{nameof(db)} is not a {nameof(DbContext)}.");
 
             switch (context.Database.ProviderName)
             {
-                case "Pomelo.EntityFrameworkCore.MySql": return queryable.MySqlMatch(query);
-                case "Microsoft.EntityFrameworkCore.Sqlite": return queryable.SqliteMatch(query);
+                case "Pomelo.EntityFrameworkCore.MySql": return queryable.MySqlMatch(path, query);
+                case "Microsoft.EntityFrameworkCore.Sqlite": return queryable.SqliteMatch(path, query);
             }
 
             throw new NotSupportedException($"Unknown database provider {context.Database.ProviderName}.");
         }
 
         //todo:
-        public static IQueryable<T> MySqlMatch<T>(this IQueryable<T> queryable, string query) => queryable;
-        public static IQueryable<T> SqliteMatch<T>(this IQueryable<T> queryable, string query) => queryable;
+        public static IQueryable<T> MySqlMatch<T, TProperty>(this IQueryable<T> queryable,
+            Expression<Func<T, TProperty>> path, string query) =>
+            queryable;
+
+        public static IQueryable<T> SqliteMatch<T, TProperty>(this IQueryable<T> queryable,
+            Expression<Func<T, TProperty>> path, string query) =>
+            queryable;
     }
 }
