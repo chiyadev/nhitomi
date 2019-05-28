235888073dba28fa35aba6e367d7f954f404e49b
phosphene47 <phosphene47@chiya.dev>
2019-05-28 16:12:28 +1000

    Add source icons to english localization

--- nhitomi/Localization/LocalizationEnglish.cs
+++ nhitomi/Localization/LocalizationEnglish.cs
@@ -80,6 +80,11 @@ namespace nhitomi.Localization
                     license = "This project is licensed under the MIT License.",
                     contribution = "Contributions are welcome! <{repoUrl}>"
                 }
+            },
+            sourceIcons = new
+            {
+                nhentai = "https://cdn.cybrhome.com/media/website/live/icon/icon_nhentai.net_57f740.png",
+                hitomi = "https://ltn.hitomi.la/favicon-160x160.png"
             }
         };
     }
