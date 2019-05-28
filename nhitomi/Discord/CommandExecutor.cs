2cd6ee087eabedbb068b8fbd42649903a6a56627
phosphene47 <phosphene47@chiya.dev>
2019-05-28 22:51:52 +1000

    Use new command parsers in CommandExecutor

--- nhitomi/Discord/CommandExecutor.cs
+++ nhitomi/Discord/CommandExecutor.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using Discord.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using nhitomi.Discord.Parsing;
 using nhitomi.Interactivity;
 
 namespace nhitomi.Discord
@@ -13,29 +16,46 @@ namespace nhitomi.Discord
     {
         readonly IServiceProvider _services;
         readonly AppSettings _settings;
-        readonly DiscordService _discord;
         readonly InteractiveManager _interactive;
         readonly ILogger<CommandExecutor> _logger;
 
-        public CommandExecutor(IServiceProvider services, IOptions<AppSettings> options, DiscordService discord,
-            InteractiveManager interactive, ILogger<CommandExecutor> logger)
+        public CommandExecutor(IServiceProvider services, IOptions<AppSettings> options, InteractiveManager interactive,
+            ILogger<CommandExecutor> logger)
         {
             _services = services;
             _settings = options.Value;
-            _discord = discord;
             _interactive = interactive;
             _logger = logger;
         }
 
-        public async Task InitializeAsync(CancellationToken cancellationToken = default)
-        {
-            using (var scope = _services.CreateScope())
-            {
-                // load command modules
-                await _discord.Command.AddModulesAsync(typeof(Startup).Assembly, scope.ServiceProvider);
-            }
+        readonly List<CommandInfo> _commands = new List<CommandInfo>();
 
-            _logger.LogDebug($"Loaded commands: {string.Join(", ", _discord.Command.Commands.Select(c => c.Name))}");
+        public Task InitializeAsync(CancellationToken cancellationToken = default)
+        {
+            // load commands
+            _commands.AddRange(typeof(Startup).Assembly
+                .GetTypes()
+                .Where(t => !t.IsAbstract && t.IsClass)
+                .SelectMany(t => t.GetMethods().Where(m => m.GetCustomAttribute<CommandAttribute>() != null))
+                .Select(t => new CommandInfo(t)));
+
+            _logger.LogDebug($"Loaded commands: {string.Join(", ", _commands.Select(c => c.Attribute.Name))}");
+
+            return Task.CompletedTask;
+        }
+
+        bool TryParseCommand(string str, out CommandInfo command, out object[] args)
+        {
+            foreach (var c in _commands)
+                if (c.TryParse(str, out args))
+                {
+                    command = c;
+                    return true;
+                }
+
+            command = null;
+            args = null;
+            return false;
         }
 
         public async Task<bool> TryHandleAsync(MessageContext context, CancellationToken cancellationToken = default)
@@ -49,27 +69,25 @@ namespace nhitomi.Discord
                     return false;
             }
 
-            var argIndex = 0;
-
-            // message has command prefix
-            if (!context.Message.HasStringPrefix(_settings.Discord.Prefix, ref argIndex) &&
-                !context.Message.HasMentionPrefix(context.Client.CurrentUser, ref argIndex))
-                return false;
-
-            IResult result;
-
-            // dependency scope
-            using (var scope = _services.CreateScope())
+            try
             {
-                // execute command
-                result = await _discord.Command.ExecuteAsync(context, argIndex, scope.ServiceProvider);
+                // message has command prefix
+                if (!context.Message.Content.StartsWith(_settings.Discord.Prefix))
+                    return false;
+
+                // parse command
+                if (!TryParseCommand(context.Message.Content, out var command, out var args))
+                    return false;
+
+                // dependency scope
+                using (var scope = _services.CreateScope())
+                {
+                    // invoke command
+                    await command.InvokeAsync(scope.ServiceProvider, args);
+                }
             }
-
-            // check for any errors during command execution
-            if (result.Error == CommandError.Exception)
+            catch (Exception e)
             {
-                var e = ((ExecuteResult) result).Exception;
-
                 _logger.LogWarning(e, "Exception while handling message {0}.", context.Message.Id);
 
                 // notify the user about this error
