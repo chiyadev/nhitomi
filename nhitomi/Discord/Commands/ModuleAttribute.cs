4d1d2f6268fc908c264d910f44f78e975af7143b
phosphene47 <phosphene47@chiya.dev>
2019-05-28 18:08:48 +1000

    Fix module attribute

--- nhitomi/Discord/Commands/ModuleAttribute.cs
+++ nhitomi/Discord/Commands/ModuleAttribute.cs
@@ -1,14 +1,19 @@
 using System;
+using System.Linq;
 
 namespace nhitomi.Discord.Commands
 {
     [AttributeUsage(AttributeTargets.Class)]
     public class ModuleAttribute : Attribute
     {
-        public string Name { get; }
+        public string[] Path { get; }
 
         public ModuleAttribute(string name)
         {
+            Path = name
+                .Split('.', StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => n.Trim().ToLowerInvariant())
+                .ToArray();
         }
     }
 }
