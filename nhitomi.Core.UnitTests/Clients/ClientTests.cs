2e4538841c4e368cfdf436a194b312f54c738e33
phosphene47 <phosphene47@chiya.dev>
2019-05-29 20:44:10 +1000

    Add doujin client unit tests

--- nhitomi.Core.UnitTests/Clients/ClientTests.cs
+++ nhitomi.Core.UnitTests/Clients/ClientTests.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
 using nhitomi.Core.Clients;
+using nhitomi.Core.Clients.Hitomi;
+using nhitomi.Core.Clients.nhentai;
 using NUnit.Framework;
 
 namespace nhitomi.Core.UnitTests.Clients
@@ -9,9 +11,31 @@ namespace nhitomi.Core.UnitTests.Clients
         [Test]
         public async Task nhentaiClient()
         {
+            var client = new nhentaiClient(
+                TestUtils.HttpClient,
+                TestUtils.Serializer,
+                TestUtils.Logger<nhentaiClient>());
+
+            await RunTestAsync(client);
+        }
+
+        [Test]
+        public async Task HitomiClient()
+        {
+            var client = new HitomiClient(
+                TestUtils.HttpClient,
+                TestUtils.Serializer,
+                TestUtils.Logger<HitomiClient>());
+
+            await RunTestAsync(client);
+        }
+
+        static async Task RunTestAsync(IDoujinClient client)
+        {
             var tester = new ClientTester();
 
-            await tester.TestAsync()
+            if (!await tester.TestAsync(client))
+                tester.ThrowExceptions();
         }
     }
 }
