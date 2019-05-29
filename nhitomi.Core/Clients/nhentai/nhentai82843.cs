848082c417c04b3bd5080bd4d7577ddd0e1a45a8
phosphene47 <phosphene47@chiya.dev>
2019-05-29 21:07:07 +1000

    Update nhentai82843

--- nhitomi.Core/Clients/nhentai/nhentai82843.cs
+++ nhitomi.Core/Clients/nhentai/nhentai82843.cs
@@ -9,6 +9,11 @@ namespace nhitomi.Core.Clients.nhentai
 
         public override DoujinInfo KnownValue { get; } = new DoujinInfo
         {
+            GalleryUrl = "https://nhentai.net/g/82843",
+            PrettyName = "Kami-sama o Chikan",
+            OriginalName = "神様を痴漢",
+            UploadTime = new DateTime(2014, 6, 28, 23, 14, 15),
+            SourceId = "82843",
             ParodyOf = "the world god only knows",
             Characters = new[]
             {
