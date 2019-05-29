d7710dd2a59721fefc7b37e207e7e8cdff9b86a4
phosphene47 <phosphene47@chiya.dev>
2019-05-29 20:39:53 +1000

    Add method to throw exceptions that occurred during client test

--- nhitomi.Core/Clients/ClientTester.cs
+++ nhitomi.Core/Clients/ClientTester.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -36,7 +37,8 @@ namespace nhitomi.Core.Clients
 
                     // compare the retrieved doujin with the known value
                     if (!doujin.Equals(testCase.KnownValue))
-                        return false;
+                        throw new ClientTesterException(
+                            $"Doujin '{testCase.DoujinId}' returned by {client.GetType().Name} did not pass test {testCase.GetType().Name}.");
                 }
 
                 return true;
@@ -51,6 +53,46 @@ namespace nhitomi.Core.Clients
                 Exceptions.Enqueue(e);
                 return false;
             }
+        }
+
+        public void ThrowExceptions()
+        {
+            var exceptions = new List<Exception>();
+
+            while (Exceptions.TryDequeue(out var exception))
+                exceptions.Add(exception);
+
+            switch (exceptions.Count)
+            {
+                case 0:
+                    return;
+
+                case 1:
+                    throw exceptions[0];
+
+                default:
+                    throw new AggregateException(exceptions);
+            }
+        }
+    }
+
+    [Serializable]
+    public class ClientTesterException : Exception
+    {
+        public ClientTesterException()
+        {
+        }
+
+        public ClientTesterException(string message) : base(message)
+        {
+        }
+
+        public ClientTesterException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected ClientTesterException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
         }
     }
 }
