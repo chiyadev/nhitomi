e3117f5189d463831eb4c5f6b586d281101807ba
phosphene47 <phosphene47@chiya.dev>
2019-05-30 19:20:28 +1000

    Improve ClientTester exceptions

--- nhitomi.Core/Clients/ClientTester.cs
+++ nhitomi.Core/Clients/ClientTester.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Threading;
@@ -33,12 +34,23 @@ namespace nhitomi.Core.Clients
                 foreach (var testCase in testCases)
                 {
                     // retrieve doujin
-                    var doujin = await client.GetAsync(testCase.DoujinId, cancellationToken);
+                    var x = await client.GetAsync(testCase.DoujinId, cancellationToken);
+                    var y = testCase.KnownValue;
 
                     // compare the retrieved doujin with the known value
-                    if (!DoujinEquals(doujin, testCase.KnownValue))
-                        throw new ClientTesterException(
-                            $"Doujin '{testCase.DoujinId}' returned by {client.GetType().Name} did not pass test {testCase.GetType().Name}.");
+                    Compare(x.GalleryUrl, y.GalleryUrl, nameof(DoujinInfo.GalleryUrl));
+                    Compare(x.PrettyName, y.PrettyName, nameof(DoujinInfo.PrettyName));
+                    Compare(x.OriginalName, y.OriginalName, nameof(DoujinInfo.OriginalName));
+                    Compare(x.UploadTime, y.UploadTime, nameof(DoujinInfo.UploadTime));
+                    Compare(x.SourceId, y.SourceId, nameof(DoujinInfo.SourceId));
+                    Compare(x.Artist, y.Artist, nameof(DoujinInfo.Artist));
+                    Compare(x.Group, y.Group, nameof(DoujinInfo.Group));
+                    Compare(x.Scanlator, y.Scanlator, nameof(DoujinInfo.Scanlator));
+                    Compare(x.Language, y.Language, nameof(DoujinInfo.Language));
+                    Compare(x.Characters, y.Characters, nameof(DoujinInfo.Characters));
+                    Compare(x.Categories, y.Categories, nameof(DoujinInfo.Categories));
+                    Compare(x.Tags, y.Tags, nameof(DoujinInfo.Tags));
+                    Compare(x.PageCount, y.PageCount, nameof(DoujinInfo.PageCount));
                 }
 
                 return true;
@@ -55,6 +67,26 @@ namespace nhitomi.Core.Clients
             }
         }
 
+        static void Compare<T>(T x, T y, string propertyName)
+        {
+            if (Equals(x, y))
+                return;
+
+            throw new ClientTesterException($"Property '{propertyName}' did not match: '{x}' != '{y}'");
+        }
+
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        static void Compare<T>(IEnumerable<T> x, IEnumerable<T> y, string propertyName) where T : IEquatable<T>
+        {
+            if (Equals(x, y))
+                return;
+            if (x != null && y != null && x.OrderlessEquals(y))
+                return;
+
+            throw new ClientTesterException($"Property '{propertyName}' did not match: " +
+                                            $"'{(x == null ? "<null>" : string.Join("', '", x))}' != '{(y == null ? "<null>" : string.Join("', '", y))}'");
+        }
+
         public void ThrowExceptions()
         {
             var exceptions = new List<Exception>();
@@ -68,28 +100,12 @@ namespace nhitomi.Core.Clients
                     return;
 
                 case 1:
-                    throw exceptions[0];
+                    throw new ClientTesterException("Exception during client testing.", exceptions[0]);
 
                 default:
                     throw new AggregateException(exceptions);
             }
         }
-
-        static bool DoujinEquals(DoujinInfo x, DoujinInfo y) =>
-            x.GalleryUrl == y.GalleryUrl &&
-            x.PrettyName == y.PrettyName &&
-            x.OriginalName == y.OriginalName &&
-            x.UploadTime == y.UploadTime &&
-            x.SourceId == y.SourceId &&
-            x.Artist == y.Artist &&
-            x.Group == y.Group &&
-            x.Scanlator == y.Scanlator &&
-            x.Language == y.Language &&
-            x.Parody == y.Parody &&
-            x.Characters.OrderlessEquals(y.Characters) &&
-            x.Categories.OrderlessEquals(y.Categories) &&
-            x.Tags.OrderlessEquals(y.Tags) &&
-            x.PageCount == y.PageCount;
     }
 
     [Serializable]
