d6bff7c2c2117695862826502abbffc6c734e1e1
phosphene47 <phosphene47@chiya.dev>
2019-05-31 12:52:36 +1000

    Increase max tag length

---
+++ nhitomi.Core/Migrations/20190531025131_IncreaseTagMaxLength.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace nhitomi.Core.Migrations
+{
+    public partial class IncreaseTagMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                "Value",
+                "Tags",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 32);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                "Value",
+                "Tags",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 64);
+        }
+    }
+}
