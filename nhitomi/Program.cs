b180d2666059eaa119b5b062e9fb67b970cf0cbc
phosphene47 <phosphene47@chiya.dev>
2019-05-31 16:43:41 +1000

    Login to api on startup

--- nhitomi/Program.cs
+++ nhitomi/Program.cs
@@ -35,16 +35,22 @@ namespace nhitomi
             readonly IHostingEnvironment _environment;
             readonly nhitomiDbContext _db;
             readonly DiscordService _discord;
+            readonly IApiClient _apiClient;
 
-            public Initialization(IHostingEnvironment environment, nhitomiDbContext db, DiscordService discord)
+            public Initialization(IHostingEnvironment environment, nhitomiDbContext db, DiscordService discord,
+                IApiClient apiClient)
             {
                 _environment = environment;
                 _db = db;
                 _discord = discord;
+                _apiClient = apiClient;
             }
 
             public async Task RunAsync(CancellationToken cancellationToken = default)
             {
+                // login to api
+                await _apiClient.LoginAsync(cancellationToken);
+
                 // migrate local database in development
                 if (_environment.IsDevelopment())
                     await _db.Database.MigrateAsync(cancellationToken);
