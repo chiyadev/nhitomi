b75a92ea24e70eff1783c0adf9479d9d17a69b76
phosphene47 <phosphene47@chiya.dev>
2019-06-02 16:42:04 +1000

    Fix FavoriteTrigger to work

--- nhitomi/Interactivity/Triggers/FavoriteTrigger.cs
+++ nhitomi/Interactivity/Triggers/FavoriteTrigger.cs
@@ -15,66 +15,83 @@ namespace nhitomi.Interactivity.Triggers
 
         public class Action : ActionBase<IDoujinMessage>
         {
-            readonly DiscordService _discord;
             readonly IDatabase _database;
 
-            public Action(DiscordService discord, IDatabase database)
+            public Action(IDatabase database)
             {
-                _discord = discord;
                 _database = database;
             }
 
-            const string _collectionName = "Favorites";
+            const string _favoritesCollection = "Favorites";
 
             public override async Task<bool> RunAsync(CancellationToken cancellationToken = default)
             {
                 if (!await base.RunAsync(cancellationToken))
                     return false;
 
-                // retrieve doujin
-                var doujin = Interactive?.Doujin;
-
-                if (doujin == null)
+                using (Context.BeginTyping())
                 {
-                    // stateless mode
-                    if (!DoujinMessage.TryParseDoujinIdFromMessage(Context.Message, out var id))
-                        return false;
-
-                    doujin = await _database.GetDoujinAsync(id.source, id.id, cancellationToken);
+                    // retrieve doujin
+                    var doujin = Interactive?.Doujin;
 
                     if (doujin == null)
-                        return false;
-                }
-
-                do
-                {
-                    var collection = await _database.GetCollectionAsync(
-                        Context.User.Id, _collectionName, cancellationToken);
-
-                    if (collection == null)
                     {
-                        // create new collection for favorites
-                        collection = new Collection
-                        {
-                            Name = _collectionName,
-                            OwnerId = Context.User.Id,
-                            Doujins = new List<CollectionRef>()
-                        };
+                        // stateless mode
+                        if (!DoujinMessage.TryParseDoujinIdFromMessage(Context.Message, out var id))
+                            return false;
 
-                        _database.Add(collection);
+                        doujin = await _database.GetDoujinAsync(id.source, id.id, cancellationToken);
+
+                        if (doujin == null)
+                            return false;
                     }
 
-                    // add to favorites collection
-                    collection.Doujins.Add(new CollectionRef
+                    do
                     {
-                        DoujinId = doujin.Id
-                    });
+                        var collection = await _database.GetCollectionAsync(
+                            Context.User.Id, _favoritesCollection, cancellationToken);
+
+                        if (collection == null)
+                        {
+                            // create new collection for favorites
+                            collection = new Collection
+                            {
+                                Name = _favoritesCollection,
+                                OwnerId = Context.User.Id,
+                                Doujins = new List<CollectionRef>()
+                            };
+
+                            _database.Add(collection);
+                        }
+
+                        // add to favorites collection
+                        collection.Doujins.Add(new CollectionRef
+                        {
+                            DoujinId = doujin.Id
+                        });
+                    }
+                    while (!await _database.SaveAsync(cancellationToken));
+
+                    try
+                    {
+                        await Context.ReplyAsync("messages.addedToCollection");
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            // try replying in DM if we don't have perms
+                            var channel = await Context.User.GetOrCreateDMChannelAsync();
+
+                            await Context.ReplyAsync(channel, "messages.addedToCollection");
+                        }
+                        catch
+                        {
+                            // ignored
+                        }
+                    }
                 }
-                while (!await _database.SaveAsync(cancellationToken));
 
-                var channel = await _discord.GetUser(Context.User.Id).GetOrCreateDMChannelAsync();
-
-                //await channel.SendMessageAsync(_formatter.AddedToCollection(_favoritesCollection, doujin));
                 return true;
             }
         }
