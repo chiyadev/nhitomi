0ae4daa2c0a5e2c088d9e02e6263c7b306bf7877
phosphene47 <phosphene47@chiya.dev>
2019-06-04 01:08:18 +1000

    Explicitly initialize DateTime

--- nhitomi.Core/Clients/Hitomi/Hitomi1422964.cs
+++ nhitomi.Core/Clients/Hitomi/Hitomi1422964.cs
@@ -13,7 +13,7 @@ namespace nhitomi.Core.Clients.Hitomi
             PrettyName = "Nama Emo - Muboubi Na JC Pri Chan Idol No Oshiego No Tame Ni Otona Chinpo De Torotoro " +
                          "Asedaku Wakarase Koubi Shidou!",
             OriginalName = "생에모 무방비한 J○ 프리챤 아이돌 제자에게 어른 자지로 찐득찐득 땀 범벅이 되는 교미 지도!",
-            UploadTime = DateTimeOffset.Parse("2019-05-30T10:25:19+00:00").UtcDateTime,
+            UploadTime = new DateTime(2019, 5, 30, 3, 52, 0, DateTimeKind.Utc),
             Artist = "tokomaya keita",
             Group = "circle tokomaya",
             Language = "korean",
