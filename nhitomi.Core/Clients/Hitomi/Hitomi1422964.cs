a768dee2e15fa0e51863ff1267319583fd2e5f71
phosphene47 <phosphene47@chiya.dev>
2019-06-04 18:32:25 +1000

    Fix hitomi test dates

--- nhitomi.Core/Clients/Hitomi/Hitomi1422964.cs
+++ nhitomi.Core/Clients/Hitomi/Hitomi1422964.cs
@@ -13,7 +13,8 @@ namespace nhitomi.Core.Clients.Hitomi
             PrettyName = "Nama Emo - Muboubi na JC Pri Chan Idol no Oshiego no Tame ni Otona Chinpo de Torotoro " +
                          "Asedaku Wakarase Koubi Shidou!",
             OriginalName = "생에모 무방비한 J○ 프리챤 아이돌 제자에게 어른 자지로 찐득찐득 땀 범벅이 되는 교미 지도!",
-            UploadTime = new DateTime(2019, 5, 30, 3, 52, 0, DateTimeKind.Utc),
+            UploadTime = new DateTime(2019, 5, 30, 8, 52, 0, DateTimeKind.Utc),
+            SourceId = "1422964",
             Artist = "tokomaya keita",
             Group = "circle tokomaya",
             Language = "korean",
