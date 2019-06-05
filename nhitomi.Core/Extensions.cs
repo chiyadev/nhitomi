ad8da1eec811cdbdae4471183358e157025e5bb7
phosphene47 <phosphene47@chiya.dev>
2019-06-05 20:20:46 +1000

    Add http message cloning extensions

--- nhitomi.Core/Extensions.cs
+++ nhitomi.Core/Extensions.cs
@@ -12,6 +12,42 @@ namespace nhitomi.Core
 {
     public static class Extensions
     {
+        public static async Task<HttpRequestMessage> CloneAsync(this HttpRequestMessage request)
+        {
+            var clone = new HttpRequestMessage(request.Method, request.RequestUri)
+            {
+                Content = await request.Content.CloneAsync().ConfigureAwait(false),
+                Version = request.Version
+            };
+
+            foreach (var prop in request.Properties)
+                clone.Properties.Add(prop);
+
+            foreach (var header in request.Headers)
+                clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
+
+            return clone;
+        }
+
+        public static async Task<HttpContent> CloneAsync(this HttpContent content)
+        {
+            if (content == null)
+                return null;
+
+            var ms = new MemoryStream();
+
+            await content.CopyToAsync(ms).ConfigureAwait(false);
+
+            ms.Position = 0;
+
+            var clone = new StreamContent(ms);
+
+            foreach (var header in content.Headers)
+                clone.Headers.Add(header.Key, header.Value);
+
+            return clone;
+        }
+
         public static bool OrderlessEquals<T>(this IEnumerable<T> enumerable, IEnumerable<T> other)
         {
             var set = new HashSet<T>(enumerable);
