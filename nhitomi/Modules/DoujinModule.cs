c1db5c309f71829a116e8d83aaffded11c1fc7c6
phosphene47 <phosphene47@chiya.dev>
2019-06-30 23:42:40 +1000

    Fix source contractions not working for doujin command overloads with two arguments
    Signed-off-by: phosphene47 <phosphene47@chiya.dev>

--- nhitomi/Modules/DoujinModule.cs
+++ nhitomi/Modules/DoujinModule.cs
@@ -32,7 +32,8 @@ namespace nhitomi.Modules
                                    string id,
                                    CancellationToken cancellationToken = default)
         {
-            var doujin = await _database.GetDoujinAsync(source, id, cancellationToken);
+            var doujin =
+                await _database.GetDoujinAsync(GalleryUtility.ExpandContraction(source), id, cancellationToken);
 
             if (doujin == null)
             {
@@ -103,7 +104,7 @@ namespace nhitomi.Modules
                 {
                     Query         = query,
                     QualityFilter = false,
-                    Source        = source
+                    Source        = GalleryUtility.ExpandContraction(source)
                 }),
                 _context,
                 cancellationToken);
@@ -131,7 +132,8 @@ namespace nhitomi.Modules
                                         string id,
                                         CancellationToken cancellationToken = default)
         {
-            var doujin = await _database.GetDoujinAsync(source, id, cancellationToken);
+            var doujin =
+                await _database.GetDoujinAsync(GalleryUtility.ExpandContraction(source), id, cancellationToken);
 
             if (doujin == null)
             {
@@ -168,7 +170,8 @@ namespace nhitomi.Modules
                                     string id,
                                     CancellationToken cancellationToken = default)
         {
-            var doujin = await _database.GetDoujinAsync(source, id, cancellationToken);
+            var doujin =
+                await _database.GetDoujinAsync(GalleryUtility.ExpandContraction(source), id, cancellationToken);
 
             if (doujin == null)
             {
