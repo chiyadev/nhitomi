1d676b4f349be6b05f895e2d078452ed689672da
phosphene47 <phosphene47@chiya.dev>
2019-07-02 21:03:40 +1000

    Automatically merge interactive states when updating too quickly
    Signed-off-by: phosphene47 <phosphene47@chiya.dev>

--- nhitomi/Interactivity/InteractiveMessage.cs
+++ nhitomi/Interactivity/InteractiveMessage.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Discord;
 using Microsoft.Extensions.DependencyInjection;
+using nhitomi.Discord;
 using nhitomi.Interactivity.Triggers;
 
 namespace nhitomi.Interactivity
@@ -17,11 +18,11 @@ namespace nhitomi.Interactivity
     public abstract class InteractiveMessage<TView> : EmbedMessage<TView>, IInteractiveMessage
         where TView : EmbedMessage<TView>.ViewBase
     {
-        readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1);
-
         public IReadOnlyDictionary<IEmote, IReactionTrigger> Triggers { get; private set; }
 
         protected abstract IEnumerable<IReactionTrigger> CreateTriggers();
+
+        readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1);
 
         public override async Task<bool> UpdateViewAsync(IServiceProvider services,
                                                          CancellationToken cancellationToken = default)
@@ -65,6 +66,96 @@ namespace nhitomi.Interactivity
                     // message was deleted
                 }
             }
+        }
+
+        readonly Queue<InteractiveViewState> _pendingStates = new Queue<InteractiveViewState>();
+
+        struct InteractiveViewState
+        {
+            public IDiscordContext Context;
+            public Optional<string> Message;
+            public Optional<Embed> Embed;
+
+            public InteractiveViewState(IDiscordContext context,
+                                        Optional<string> message,
+                                        Optional<Embed> embed)
+            {
+                Context = context;
+                Message = message;
+                Embed   = embed;
+            }
+        }
+
+        DateTime _lastUpdateTime;
+
+        protected override async Task UpdateMessageAsync(IDiscordContext context,
+                                                         Optional<string> message,
+                                                         Optional<Embed> embed,
+                                                         CancellationToken cancellationToken = default)
+        {
+            var currentTime = DateTime.Now;
+
+            // always queue state if other states are pending
+            lock (_pendingStates)
+            {
+                if (_pendingStates.Count > 0)
+                {
+                    _pendingStates.Enqueue(new InteractiveViewState(context, message, embed));
+
+                    return;
+                }
+            }
+
+            var timeSinceLastUpdate = currentTime - _lastUpdateTime;
+
+            // if updating quickly (more than once per second)
+            if (timeSinceLastUpdate < TimeSpan.FromSeconds(1))
+            {
+                // queue state to update in the background
+                lock (_pendingStates)
+                    _pendingStates.Enqueue(new InteractiveViewState(context, message, embed));
+
+                // ReSharper disable once RedundantArgumentDefaultValue
+                _ = Task.Run(() => UpdateStateAsync(timeSinceLastUpdate, default), cancellationToken);
+            }
+
+            // otherwise update immediately
+            else
+            {
+                await base.UpdateMessageAsync(context, message, embed, cancellationToken);
+            }
+
+            _lastUpdateTime = currentTime;
+        }
+
+        async Task UpdateStateAsync(TimeSpan initialDelay,
+                                    CancellationToken cancellationToken = default)
+        {
+            await Task.Delay(initialDelay, cancellationToken);
+
+            var state = new InteractiveViewState(null,
+                                                 Optional<string>.Unspecified,
+                                                 Optional<Embed>.Unspecified);
+
+            lock (_pendingStates)
+            {
+                if (_pendingStates.Count == 0)
+                    return;
+
+                // merge pending states into one
+                while (_pendingStates.TryDequeue(out var s))
+                {
+                    state.Context = s.Context;
+
+                    if (s.Message.IsSpecified)
+                        state.Message = s.Message;
+
+                    if (s.Embed.IsSpecified)
+                        state.Embed = s.Embed;
+                }
+            }
+
+            await base.UpdateMessageAsync(state.Context, state.Message, state.Embed, cancellationToken);
         }
 
         public virtual void Dispose() => _semaphore.Dispose();
