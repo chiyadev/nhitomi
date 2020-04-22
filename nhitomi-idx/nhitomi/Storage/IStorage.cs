b0914b4f03f5f089e697eb31ca6cbfa8302e31b6
phosphene47 <phosphene47@chiya.dev>
2020-04-23 02:44:08 +1000

    Add string read/write methods on storage

--- nhitomi-idx/nhitomi/Storage/IStorage.cs
+++ nhitomi-idx/nhitomi/Storage/IStorage.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using OneOf;
 using OneOf.Types;
 
@@ -33,5 +36,49 @@ namespace nhitomi.Storage
         /// Deletes multiple files from this storage. This does not guarantee atomicity.
         /// </summary>
         Task DeleteAsync(string[] names, CancellationToken cancellationToken = default);
+
+        async Task<string> ReadStringAsync(string name, CancellationToken cancellationToken = default, Encoding encoding = null)
+        {
+            var data = null as string;
+
+            var result = await ReadAsync(name, cancellationToken);
+
+            if (result.TryPickT0(out var file, out _))
+                data = (encoding ?? Encoding.UTF8).GetString(await file.Stream.ToArrayAsync(cancellationToken));
+
+            return string.IsNullOrEmpty(data) ? null : data;
+        }
+
+        Task WriteStringAsync(string name, string data, CancellationToken cancellationToken = default, Encoding encoding = null)
+        {
+            if (string.IsNullOrEmpty(data))
+                return Task.CompletedTask;
+
+            using var file = new StorageFile
+            {
+                Name      = name,
+                Stream    = new MemoryStream((encoding ?? Encoding.UTF8).GetBytes(data)),
+                MediaType = "text/plain"
+            };
+
+            return WriteAsync(file, cancellationToken);
+        }
+
+        async Task<T> ReadObjectAsync<T>(string name, CancellationToken cancellationToken = default, Encoding encoding = null)
+        {
+            var data = await ReadStringAsync(name, cancellationToken, encoding);
+
+            if (data == null)
+                return default;
+
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        Task WriteObjectAsync<T>(string name, T value, CancellationToken cancellationToken = default, Encoding encoding = null)
+        {
+            var data = JsonConvert.SerializeObject(value);
+
+            return WriteStringAsync(name, data, cancellationToken, encoding);
+        }
     }
 }
