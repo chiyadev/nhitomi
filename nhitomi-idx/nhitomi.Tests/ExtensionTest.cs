9de8c2df1d0ad394c49e3d310e0bf49acdff7a29
phosphene47 <phosphene47@chiya.dev>
2020-04-22 17:52:40 +1000

    Add more extension method tests

--- nhitomi-idx/nhitomi.Tests/ExtensionTest.cs
+++ nhitomi-idx/nhitomi.Tests/ExtensionTest.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace nhitomi
@@ -38,6 +42,63 @@ namespace nhitomi
             var my = new[] { MyEnum.One, MyEnum.Two, MyEnum.None, MyEnum.OneAndTwo, MyEnum.None }.ToBitwise();
 
             Assert.That(my, Is.EqualTo(MyEnum.One | MyEnum.Two));
+        }
+
+        [Test]
+        public void BufferEqualsTrue()
+        {
+            var a = new byte[] { 1, 2, 3, 4, 5 };
+            var b = new byte[] { 1, 2, 3, 4, 5 };
+
+            Assert.That(a.BufferEquals(b), Is.True);
+
+            b[2] = 0;
+
+            Assert.That(a.BufferEquals(b), Is.False);
+        }
+
+        [Test]
+        public void BufferSubset()
+        {
+            var a = new HashSet<byte[]>
+            {
+                new byte[] { 1, 2, 3 },
+                new byte[] { 4, 5, 6 }
+            };
+
+            var b = new HashSet<byte[]>
+            {
+                new byte[] { 1, 2, 3 },
+                new byte[] { 4, 5, 6 },
+                new byte[] { 7, 8, 9 }
+            };
+
+            Assert.That(a.BufferSubset(b), Is.True);
+            Assert.That(b.BufferSubset(a), Is.False);
+        }
+
+        [Test]
+        public async Task SemaphoreDisposeInLock()
+        {
+            using var semaphore = new SemaphoreSlim(1);
+
+            using (await semaphore.EnterAsync())
+                semaphore.Dispose();
+        }
+
+        public enum MyNamedEnum
+        {
+            [EnumMember(Value = "named")] Named,
+            [EnumMember] Invalid,
+            Unnamed
+        }
+
+        [Test]
+        public void GetEnumName()
+        {
+            Assert.That(MyNamedEnum.Named.GetEnumName(), Is.EqualTo("named"));
+            Assert.That(MyNamedEnum.Invalid.GetEnumName(), Is.EqualTo(nameof(MyNamedEnum.Invalid)));
+            Assert.That(MyNamedEnum.Unnamed.GetEnumName(), Is.EqualTo(nameof(MyNamedEnum.Unnamed)));
         }
     }
 }
