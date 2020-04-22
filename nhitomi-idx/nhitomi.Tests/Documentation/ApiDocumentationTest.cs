77824ed3d5f997676808d9111fcbcdcd2d099aaf
phosphene47 <phosphene47@chiya.dev>
2020-04-22 17:31:50 +1000

    Ensure generated spec is valid json

--- nhitomi-idx/nhitomi.Tests/Documentation/ApiDocumentationTest.cs
+++ nhitomi-idx/nhitomi.Tests/Documentation/ApiDocumentationTest.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace nhitomi.Documentation
@@ -7,7 +8,7 @@ namespace nhitomi.Documentation
     public class ApiDocumentationTest : TestBaseHttpClient
     {
         [Test]
-        public Task Specs() => GetAsync<object>("docs/v1.json");
+        public Task Specs() => GetAsync<JObject>("docs/v1.json");
 
         [Test]
         public async Task Docs() => (await RequestAsync(HttpMethod.Get, "docs/index.html", null)).Dispose();
