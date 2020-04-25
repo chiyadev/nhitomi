f849dd7e520ed687b8424d16f5584440456892fe
phosphene47 <phosphene47@chiya.dev>
2020-04-25 20:23:40 +1000

    Add typed overloads for result utils

--- nhitomi-idx/nhitomi/ResultUtilities.cs
+++ nhitomi-idx/nhitomi/ResultUtilities.cs
@@ -24,7 +24,7 @@ namespace nhitomi
             };
         }
 
-        public static ActionResult NotFound<T>(params object[] ids) => Status(HttpStatusCode.NotFound, $"{typeof(T).Name} '{string.Join('/', ids)}' not found.");
+        public static ActionResult NotFound(params object[] ids) => Status(HttpStatusCode.NotFound, $"'{string.Join('/', ids)}' not found.");
         public static ActionResult Forbidden(string message) => Status(HttpStatusCode.Forbidden, message);
         public static ActionResult BadRequest(string message) => Status(HttpStatusCode.BadRequest, message);
         public static ActionResult Unauthorized(string message) => Status(HttpStatusCode.Unauthorized, message);
@@ -37,5 +37,11 @@ namespace nhitomi
 
             return Value(HttpStatusCode.UnprocessableEntity, problems, message);
         }
+
+        public static ActionResult<T> NotFound<T>(params object[] ids) => NotFound(ids);
+        public static ActionResult<T> Forbidden<T>(string message) => Forbidden(message);
+        public static ActionResult<T> BadRequest<T>(string message) => BadRequest(message);
+        public static ActionResult<T> Unauthorized<T>(string message) => Unauthorized(message);
+        public static ActionResult<T> UnprocessableEntity<T>(params ValidationProblem[] problems) => UnprocessableEntity(problems);
     }
 }
