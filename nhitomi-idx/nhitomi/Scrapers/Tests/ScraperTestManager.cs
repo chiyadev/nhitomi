7d9d92ec5c81e755931888ad08069d15944672c0
phosphene47 <phosphene47@chiya.dev>
2020-04-25 15:36:24 +1000

    Make test manager parameters available in interface

--- nhitomi-idx/nhitomi/Scrapers/Tests/ScraperTestManager.cs
+++ nhitomi-idx/nhitomi/Scrapers/Tests/ScraperTestManager.cs
@@ -10,14 +10,24 @@ namespace nhitomi.Scrapers.Tests
 {
     public interface IScraperTestManager
     {
+        /// <summary>
+        /// Number of tests to pick per run, or null to use all tests.
+        /// </summary>
+        int? RunTestCount { get; set; }
+
+        /// <summary>
+        /// If true, always retry failed tests before other tests.
+        /// </summary>
+        bool RetryFailed { get; set; }
+
         Task RunAsync(CancellationToken cancellationToken = default);
     }
 
-    public class ScraperTestCollection<T> : IScraperTestManager
+    public class ScraperTestManager<T> : IScraperTestManager
     {
         public IReadOnlyCollection<ScraperTest<T>> Tests { get; }
 
-        public ScraperTestCollection(IScraper scraper)
+        public ScraperTestManager(IScraper scraper)
         {
             Tests = GetType()
                    .Assembly.GetTypes()
@@ -27,32 +37,20 @@ namespace nhitomi.Scrapers.Tests
         }
 
         readonly Random _rand = new Random();
-
-        public IEnumerable<ScraperTest<T>> Pick(int count)
-        {
-            lock (_rand)
-                return Tests.OrderBy(x => _rand.Next()).Take(count).ToArray();
-        }
-
-        /// <summary>
-        /// Number of tests to pick per run.
-        /// </summary>
-        public int RunTestCount { get; set; } = 1;
-
-        /// <summary>
-        /// If true, always retry failed tests before other tests.
-        /// </summary>
-        public bool RetryFailed { get; set; } = true;
-
         readonly ConcurrentQueue<ScraperTest<T>> _failed = new ConcurrentQueue<ScraperTest<T>>();
+
+        public int? RunTestCount { get; set; } = 1;
+        public bool RetryFailed { get; set; } = true;
 
         public async Task RunAsync(CancellationToken cancellationToken = default)
         {
-            var tests = new HashSet<ScraperTest<T>>(RunTestCount);
+            var tests = new HashSet<ScraperTest<T>>(RunTestCount ?? Tests.Count);
 
+            // add previously failed tests first
             while (tests.Count < RunTestCount && _failed.TryDequeue(out var test))
                 tests.Add(test);
 
+            // add other tests
             lock (_rand)
             {
                 foreach (var test in Tests.OrderBy(x => _rand.Next()))
