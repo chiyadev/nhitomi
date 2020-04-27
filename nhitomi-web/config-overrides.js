8a198d0293075803b23d4f50777a231029fb7e04
phosphene47 <phosphene47@chiya.dev>
2020-04-27 23:41:39 +1000

    Replace react with preact

--- nhitomi-web/config-overrides.js
+++ nhitomi-web/config-overrides.js
@@ -1,8 +1,13 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra')
 
 const colors = require('./src/colors.json')
 
 module.exports = override(
+  addWebpackAlias({
+    'react': 'preact/compat',
+    'react-dom': 'preact/compat'
+  }),
+
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
