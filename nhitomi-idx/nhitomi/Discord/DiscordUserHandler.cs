81f0ff0ace4069116511a5b9d8274f1af5d9a09b
phosphene47 <phosphene47@chiya.dev>
2020-04-28 02:26:09 +1000

    Improve performance of discord user handler by caching user id

--- nhitomi-idx/nhitomi/Discord/DiscordUserHandler.cs
+++ nhitomi-idx/nhitomi/Discord/DiscordUserHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using nhitomi.Controllers;
@@ -13,21 +14,34 @@ namespace nhitomi.Discord
     public class DiscordUserHandler : IDiscordUserHandler
     {
         readonly IDiscordOAuthHandler _oauth;
+        readonly IUserService _users;
+        readonly ICacheStore<string> _userIdCache;
 
-        public DiscordUserHandler(IDiscordOAuthHandler oauth)
+        public DiscordUserHandler(IDiscordOAuthHandler oauth, IUserService users, ICacheManager cache)
         {
-            _oauth = oauth;
+            _oauth       = oauth;
+            _users       = users;
+            _userIdCache = cache.CreateStore<string>(k => $"discord:uid:{k}", TimeSpan.FromHours(1));
         }
 
         public async Task SetAsync(nhitomiCommandContext context, CancellationToken cancellationToken = default)
         {
-            // this isn't an actual oauth, but it creates users correctly
-            var user = await _oauth.GetOrCreateUserAsync(new DiscordOAuthUser
+            // try getting cached db user id from discord id
+            var id     = await _userIdCache.GetAsync(context.Executor.Id.ToString(), null, cancellationToken);
+            var result = await _users.GetAsync(id, cancellationToken);
+
+            if (!result.TryPickT0(out var user, out _))
             {
-                Id            = context.Executor.Id,
-                Username      = context.Executor.Username,
-                Discriminator = context.Executor.DiscriminatorValue
-            }, cancellationToken);
+                // this isn't an actual oauth, but it creates users correctly
+                user = await _oauth.GetOrCreateUserAsync(new DiscordOAuthUser
+                {
+                    Id            = context.Executor.Id,
+                    Username      = context.Executor.Username,
+                    Discriminator = context.Executor.DiscriminatorValue
+                }, cancellationToken);
+
+                await _userIdCache.SetAsync(context.Executor.Id.ToString(), user.Id, cancellationToken);
+            }
 
             context.User   = user;
             context.Locale = Locales.Get(user.Language).Sections["discord"];
