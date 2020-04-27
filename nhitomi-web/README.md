2076499661fd97dbe14026bd4a35d66e727ecdc9
phosphene47 <phosphene47@chiya.dev>
2020-04-28 03:19:39 +1000

    Use node to generate api client

--- nhitomi-web/README.md
+++ nhitomi-web/README.md
@@ -22,11 +22,7 @@ $ npm run genclient
 nhitomi-web uses a Typescript API client automatically generated using [openapi-generator](https://github.com/OpenAPITools/openapi-generator). When the API specification changes, you should update your API client.
 
 ```shell
-$ npm run genclient
+$ npm run genclient ["local" | <spec path>]
 ```
 
-If you are running nhitomi-idx together with nhitomi-web, you can also update the client using locally generated specification, assuming the server is running on port 5000.
-
-```shell
-$ npm run genclient:local
-```
+- If you are running nhitomi-idx on localhost together with nhitomi-web, you can generate the client with `local` argument assuming the server is running on port 5000.
---
+++ nhitomi-web/genclient.js
@@ -0,0 +1,43 @@
+const rimraf = require("rimraf")
+const { exec } = require('child_process')
+const { promisify } = require('util')
+
+let source = process.argv[2]
+
+switch (source) {
+  case undefined:
+    source = 'https://nhitomi-new.chiya.dev/api/v1/docs.json'
+    break
+
+  case 'local':
+    source = 'http://localhost:5000/api/v1/docs.json'
+    break
+}
+
+const name = 'nhitomi-api'
+
+rimraf(`./${name}`, async () => {
+  // generate client
+  console.log('Generating client using spec:', source)
+
+  await promisify(exec)(
+    `npx openapi-generator generate \
+      -psupportsES6=true \
+      -ptypescriptThreePlus=true \
+      -pprefixParameterInterfaces=true \
+      -pnpmName=${name} \
+      -pnpmRepository=https://github.com/chiyadev/nhitomi \
+      -i ${source} \
+      -g typescript-fetch \
+      -o ./${name}`)
+
+  // build package
+  console.log('Building package...')
+
+  await promisify(exec)(`cd ./${name} && npm run build`)
+
+  // save package
+  console.log('Installing package...')
+
+  await promisify(exec)(`npm install --no-save ./${name}`)
+})
