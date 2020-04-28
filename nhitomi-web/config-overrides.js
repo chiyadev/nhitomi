c5c38798087c8eae30bfb728be6ad66ac14cab63
phosphene47 <phosphene47@chiya.dev>
2020-04-28 12:49:21 +1000

    Copy locales into src for webpack import

--- nhitomi-web/config-overrides.js
+++ nhitomi-web/config-overrides.js
@@ -1,20 +1,27 @@
 const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra')
-const theme = require('@ant-design/dark-theme')
+const symlinkDir = require('symlink-dir')
 
+// symlink locale folder into src for webpack import
+symlinkDir('../locales', './src/locales')
+
+const theme = require('@ant-design/dark-theme')
 const colors = require('./src/colors.json')
 
 module.exports = override(
+  // replace react with preact
   addWebpackAlias({
     'react': 'preact/compat',
     'react-dom': 'preact/compat'
   }),
 
+  // required for antd
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: true
   }),
 
+  // theme configuration
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: {
