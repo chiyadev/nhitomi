619a2c855631cca5a38023c5aedd75b1cb2c3e08
phosphene47 <phosphene47@chiya.dev>
2020-04-29 21:32:08 +1000

    Disable service worker

--- nhitomi-web/src/index.tsx
+++ nhitomi-web/src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { register as registerServiceWorker } from './serviceWorker'
 import { App } from './App'
 
 import './localStorage'
 import './logging'
 import './colors'
-
-registerServiceWorker()
 
 ReactDOM.render(<App />, document.getElementById('root'))
