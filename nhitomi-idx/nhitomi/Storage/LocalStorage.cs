b92b9ddd33b275f2a3508c0335b29d549c6cc515
phosphene47 <phosphene47@chiya.dev>
2020-05-01 23:52:33 +1000

    Significantly improve performance of local storage by avoiding resource lock

--- nhitomi-idx/nhitomi/Storage/LocalStorage.cs
+++ nhitomi-idx/nhitomi/Storage/LocalStorage.cs
@@ -33,20 +33,51 @@ namespace nhitomi.Storage
             public string MediaType { get; set; }
         }
 
-        readonly IResourceLocker _locker;
         readonly ILogger<LocalStorage> _logger;
-
         readonly string _basePath;
 
         public LocalStorage(IHostEnvironment environment, LocalStorageOptions options, IResourceLocker locker, ILogger<LocalStorage> logger)
         {
-            _locker = locker;
-            _logger = logger;
-
+            _logger   = logger;
             _basePath = Path.Combine(environment.ContentRootPath, options.Path);
         }
 
         Task IStorage.InitializeAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        string GetPath(string name)
+        {
+            var path = Path.Combine(_basePath, name.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar));
+
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+            return path;
+        }
+
+        const int _fileRetryCount = 10;
+
+        static async Task<FileStream> OpenFileAsync(string path, FileMode mode, FileAccess access, FileShare share, CancellationToken cancellationToken = default)
+        {
+            var retry = 0;
+
+            do
+            {
+                try
+                {
+                    if (!File.Exists(path))
+                        throw new FileNotFoundException($"'{path}' not found.");
+
+                    return FileEx.OpenAsync(path, mode, access, share);
+                }
+                catch (IOException)
+                {
+                    if (++retry >= _fileRetryCount)
+                        throw;
+
+                    await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken);
+                }
+            }
+            while (true);
+        }
 
         public async Task<OneOf<StorageFile, NotFound, Exception>> ReadAsync(string name, CancellationToken cancellationToken = default)
         {
@@ -56,12 +87,9 @@ namespace nhitomi.Storage
             {
                 byte[] data;
 
-                await using (await _locker.EnterAsync($"storage:{path}", cancellationToken))
-                {
-                    await using var stream = FileEx.OpenAsync(path, FileMode.Open, FileAccess.Read);
-
+                // concurrent read possible
+                await using (var stream = await OpenFileAsync(path, FileMode.Open, FileAccess.Read, FileShare.Read, cancellationToken))
                     data = await stream.ToArrayAsync(cancellationToken);
-                }
 
                 var container = MessagePackSerializer.Deserialize<FileContainer>(data, _serializerOptions);
 
@@ -81,6 +109,28 @@ namespace nhitomi.Storage
                 _logger.LogWarning(e, $"Failed to read file '{name}'.");
                 return e;
             }
+        }
+
+        static async Task MoveFileAsync(string src, string dst, CancellationToken cancellationToken = default)
+        {
+            var retry = 0;
+
+            do
+            {
+                try
+                {
+                    File.Move(src, dst, true);
+                    return;
+                }
+                catch (IOException)
+                {
+                    if (++retry >= _fileRetryCount)
+                        throw;
+
+                    await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken);
+                }
+            }
+            while (true);
         }
 
         public async Task<OneOf<Success, Exception>> WriteAsync(StorageFile file, CancellationToken cancellationToken = default)
@@ -105,8 +155,7 @@ namespace nhitomi.Storage
                     }
 
                     // move temp file to storage
-                    await using (await _locker.EnterAsync($"storage:{path}", cancellationToken))
-                        File.Move(temp.Path, path, true);
+                    await MoveFileAsync(temp.Path, path, cancellationToken);
                 }
 
                 _logger.LogInformation($"Wrote file '{file.Name}'.");
@@ -120,14 +169,33 @@ namespace nhitomi.Storage
             }
         }
 
+        static async Task DeleteFileAsync(string path, CancellationToken cancellationToken = default)
+        {
+            var retry = 0;
+
+            do
+            {
+                try
+                {
+                    File.Delete(path);
+                    return;
+                }
+                catch (IOException)
+                {
+                    if (++retry >= _fileRetryCount)
+                        throw;
+
+                    await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken);
+                }
+            }
+            while (true);
+        }
+
         public Task DeleteAsync(string[] names, CancellationToken cancellationToken = default) => Task.WhenAll(names.Select(async name =>
         {
             try
             {
-                var path = GetPath(name);
-
-                await using (await _locker.EnterAsync($"storage:{path}", cancellationToken))
-                    File.Delete(path);
+                await DeleteFileAsync(GetPath(name), cancellationToken);
 
                 _logger.LogInformation($"Deleted file '{name}'.");
             }
@@ -136,15 +204,6 @@ namespace nhitomi.Storage
                 _logger.LogWarning(e, $"Failed to delete file '{name}'.");
             }
         }));
-
-        string GetPath(string name)
-        {
-            var path = Path.Combine(_basePath, name.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar));
-
-            Directory.CreateDirectory(Path.GetDirectoryName(path));
-
-            return path;
-        }
 
         public void Dispose() { }
     }
