166b84dacc77b0e4bffac2859f9c22d7fe87b759
phosphene47 <phosphene47@chiya.dev>
2020-05-02 00:44:22 +1000

    Fix zooming on mobile breaking

--- nhitomi-web/src/LayoutContext.tsx
+++ nhitomi-web/src/LayoutContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useMemo, useState } from 'react'
+import React, { createContext, ReactNode, useMemo, useState, useRef } from 'react'
 import { useWindowSize } from 'react-use'
 import { SideBarWidth } from './Sidebar'
 
@@ -15,7 +15,16 @@ export const LayoutContext = createContext<{
 const mdBreakpoint = 768
 
 export const LayoutProvider = ({ children }: { children?: ReactNode }) => {
-  const { width: windowWidth, height: windowHeight } = useWindowSize()
+  let { width: windowWidth, height: windowHeight } = useWindowSize()
+
+  // use an element to measure window size if possible
+  // this excludes scrollbar from width and allows zooming to work on mobile without breaking
+  const ref = useRef<HTMLDivElement>(null)
+
+  if (ref.current) {
+    windowWidth = ref.current.clientWidth
+    windowHeight = ref.current.clientHeight
+  }
 
   const breakpoint = windowWidth < mdBreakpoint
   const [sidebar, setSidebar] = useState(!breakpoint)
@@ -27,19 +36,30 @@ export const LayoutProvider = ({ children }: { children?: ReactNode }) => {
   const width = sidebar && !breakpoint ? windowWidth - SideBarWidth : windowWidth
   const height = windowHeight
 
-  return <LayoutContext.Provider children={children} value={useMemo(() => ({
-    width,
-    height,
-    mobile,
-    breakpoint,
-    sidebar,
-    setSidebar
-  }), [
-    width,
-    height,
-    mobile,
-    breakpoint,
-    sidebar,
-    setSidebar
-  ])} />
+  return <>
+    <div ref={ref} id='measurer' style={{
+      pointerEvents: 'none',
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: '100%',
+      height: '100%'
+    }} />
+
+    <LayoutContext.Provider children={children} value={useMemo(() => ({
+      width,
+      height,
+      mobile,
+      breakpoint,
+      sidebar,
+      setSidebar
+    }), [
+      width,
+      height,
+      mobile,
+      breakpoint,
+      sidebar,
+      setSidebar
+    ])} />
+  </>
 }
