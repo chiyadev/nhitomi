00d6401f7583e3bf0bcb0f266721eff09909089e
phosphene47 <phosphene47@chiya.dev>
2020-05-09 19:53:15 +1000

    Implement internal authentication endpoints

--- nhitomi-idx/nhitomi/Controllers/InternalController.cs
+++ nhitomi-idx/nhitomi/Controllers/InternalController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -11,40 +12,49 @@ namespace nhitomi.Controllers
     /// <summary>
     /// Contains endpoints used internally by nhitomi.
     /// </summary>
+    /// <remarks>
+    /// Regular users should ignore these endpoints because they will not be able to access them.
+    /// </remarks>
     [Route("internal")]
     public class InternalController : nhitomiControllerBase
     {
         readonly IConfiguration _config;
         readonly ServerOptions _serverOptions;
         readonly IElasticClient _elastic;
+        readonly IUserService _users;
+        readonly IAuthService _auth;
+        readonly IDiscordOAuthHandler _discord;
 
-        public InternalController(IConfiguration config, IOptionsSnapshot<ServerOptions> serverOptions, IElasticClient elastic)
+        public InternalController(IConfiguration config, IOptionsSnapshot<ServerOptions> serverOptions, IElasticClient elastic, IUserService users, IAuthService auth, IDiscordOAuthHandler discord)
         {
             _config        = config;
             _serverOptions = serverOptions.Value;
             _elastic       = elastic;
+            _users         = users;
+            _auth          = auth;
+            _discord       = discord;
         }
 
         /// <summary>
         /// Retrieves internal server configuration values.
         /// </summary>
         /// <remarks>
-        /// This requires <see cref="UserPermissions.ManageServer"/> permission.
+        /// Requires <see cref="UserPermissions.ManageServer"/> permission.
         /// </remarks>
-        [HttpGet("internal/config", Name = "getServerConfig"), RequireUser(Permissions = UserPermissions.ManageServer)]
-        public Dictionary<string, string> GetServerConfig()
+        [HttpGet("config", Name = "getServerConfig"), RequireUser(Permissions = UserPermissions.ManageServer)]
+        public Dictionary<string, string> GetConfig()
             => _config.Get<Dictionary<string, string>>();
 
         /// <summary>
         /// Updates internal server configuration values.
         /// </summary>
         /// <remarks>
-        /// This requires <see cref="UserPermissions.ManageServer"/> permission.
+        /// Requires <see cref="UserPermissions.ManageServer"/> permission.
         /// Changes may not take effect immediately. Some changes will require a full server restart.
         /// </remarks>
         /// <param name="data">New configuration object to replace with.</param>
-        [HttpPut("internal/config", Name = "setServerConfig"), RequireUser(Permissions = UserPermissions.ManageServer)]
-        public async Task<Dictionary<string, string>> SetServerConfig(Dictionary<string, string> data)
+        [HttpPut("config", Name = "setServerConfig"), RequireUser(Permissions = UserPermissions.ManageServer)]
+        public async Task<Dictionary<string, string>> SetConfigAsync(Dictionary<string, string> data)
         {
             var entry = _elastic.Entry<DbCompositeConfig>(DbCompositeConfig.DefaultId);
 
@@ -55,7 +65,73 @@ namespace nhitomi.Controllers
             await entry.UpdateAsync();
             await Task.Delay(_serverOptions.ConfigurationReloadInterval);
 
-            return GetServerConfig();
+            return GetConfig();
+        }
+
+        /// <summary>
+        /// Authenticates a user bypassing OAuth2.
+        /// </summary>
+        /// <remarks>
+        /// Requires <see cref="UserPermissions.CreateUsers"/> permission.
+        /// </remarks>
+        /// <param name="id">User ID.</param>
+        [HttpGet("auth/direct/{id}", Name = "authenticateUserDirect"), RequireUser(Permissions = UserPermissions.CreateUsers)]
+        public async Task<ActionResult<UserController.AuthenticateResponse>> AuthDirectAsync(string id)
+        {
+            var result = await _users.GetAsync(id);
+
+            if (!result.TryPickT0(out var user, out _))
+                return ResultUtilities.NotFound(id);
+
+            return new UserController.AuthenticateResponse
+            {
+                Token = await _auth.GenerateTokenAsync(user),
+                User  = ProcessUser(user.Convert())
+            };
+        }
+
+        public class GetOrCreateDiscordUserRequest
+        {
+            [Required]
+            public ulong Id { get; set; }
+
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            public int Discriminator { get; set; }
+
+            [Required]
+            public string Locale { get; set; }
+
+            [Required]
+            public string Email { get; set; }
+        }
+
+        /// <summary>
+        /// Gets or creates a user directly using Discord OAuth2 connection information.
+        /// </summary>
+        /// <remarks>
+        /// Requires <see cref="UserPermissions.CreateUsers"/> permission.
+        /// </remarks>
+        /// <param name="request">Discord connection information.</param>
+        [HttpPost("auth/discord", Name = "getOrCreateUserDiscord"), RequireUser(Permissions = UserPermissions.CreateUsers)]
+        public async Task<UserController.AuthenticateResponse> GetOrCreateDiscordUserAsync(GetOrCreateDiscordUserRequest request)
+        {
+            var user = await _discord.GetOrCreateUserAsync(new DiscordOAuthUser
+            {
+                Id            = request.Id,
+                Username      = request.Username,
+                Discriminator = request.Discriminator,
+                Locale        = request.Locale,
+                Email         = request.Email
+            });
+
+            return new UserController.AuthenticateResponse
+            {
+                Token = await _auth.GenerateTokenAsync(user),
+                User  = ProcessUser(user.Convert())
+            };
         }
     }
 }
