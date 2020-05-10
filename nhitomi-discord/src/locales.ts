2cbb94c65160b36895705176ea769288c5eb9348
phosphene47 <phosphene47@chiya.dev>
2020-05-11 03:47:07 +1000

    Implement nested locales

--- nhitomi-discord/src/locales.ts
+++ nhitomi-discord/src/locales.ts
@@ -19,20 +19,37 @@ export class Locale {
 
   readonly locale: string
 
-  private constructor(readonly language: LanguageType) {
+  protected constructor(readonly language: LanguageType) {
     this.locale = language.toString()
   }
 
   /** Selects a message given a message key and formats it using the given replacements. */
-  l(key: string, values?: Record<string, any>): string {
+  get(key: string, values?: Record<string, any>): string {
     let result = __mf({ phrase: key, locale: this.locale }, values)
 
     // until key-level fallback is implemented, we use this hack to fallback to default language.
     // https://github.com/mashpie/i18n-node/issues/80
     // https://github.com/mashpie/i18n-node/issues/167
     if (result === key)
-      result = Locale.default.l(key, values)
+      result = Locale.default.get(key, values)
 
     return result
+  }
+
+  section(name: string): Locale {
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    return new LocaleSection(this, name + '.')
+  }
+}
+
+class LocaleSection extends Locale {
+  constructor(readonly inner: Locale, readonly prefix: string) { super(inner.language) }
+
+  get(key: string, values?: Record<string, any>): string {
+    return this.inner.get(this.prefix + key, values)
+  }
+
+  section(name: string): Locale {
+    return new LocaleSection(this.inner, name + '.' + this.prefix)
   }
 }
