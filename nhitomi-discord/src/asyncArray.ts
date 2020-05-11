3555a7219f502ae16aa6ada1c2ebac7ba0ce8332
phosphene47 <phosphene47@chiya.dev>
2020-05-12 01:23:26 +1000

    Ensure undefined item does not get fetched more than once

--- nhitomi-discord/src/asyncArray.ts
+++ nhitomi-discord/src/asyncArray.ts
@@ -1,6 +1,6 @@
 /** An array that loads items asynchronously in chunks and caches them in memory. Used to display paginated search results. */
 export class AsyncArray<T> {
-  readonly cache: T[] = []
+  readonly cache: ({ loaded: true, value: T | undefined } | undefined)[] = []
 
   get loadedLength(): number {
     for (let i = this.cache.length - 1; i >= 0; i--)
@@ -12,22 +12,34 @@ export class AsyncArray<T> {
 
   constructor(
     readonly chunkSize: number,
-    readonly onload: (offset: number, limit: number) => Promise<T[]>
+    readonly fetch: (offset: number, limit: number) => Promise<T[]>
   ) { }
 
   async get(index: number): Promise<T | undefined> {
+    let current = this.cache[index]
+
+    if (current)
+      return current.value
+
+    const start = Math.floor(index / this.chunkSize) * this.chunkSize
+    const loaded = await this.fetch(start, this.chunkSize)
+
+    for (let i = 0; i < Math.max(loaded.length, this.chunkSize); i++)
+      this.cache[start + i] = { loaded: true, value: loaded[i] }
+
+    current = this.cache[index]
+
+    if (current)
+      return current.value
+
+    this.cache[index] = { loaded: true, value: undefined }
+  }
+
+  getCached(index: number): T | undefined {
     const current = this.cache[index]
 
     if (current)
-      return current
-
-    const start = Math.floor(index / this.chunkSize) * this.chunkSize
-    const loaded = await this.onload(start, this.chunkSize)
-
-    for (let i = 0; i < loaded.length; i++)
-      this.cache[start + i] = loaded[i]
-
-    return this.cache[index]
+      return current.value
   }
 
   reset(): void { this.cache.length = 0 }
