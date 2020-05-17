586dc2de31d0f4e14608d1869c3b479e1ed05b46
phosphene47 <phosphene47@chiya.dev>
2020-05-18 01:01:23 +1000

    Update deprecation message

--- nhitomi-discord/src/Commands/download.ts
+++ nhitomi-discord/src/Commands/download.ts
@@ -4,7 +4,7 @@ import { run as runInternal } from './get'
 // todo: remove this deprecation notice completely in August 2020
 
 export const run: CommandFunc = async (context, arg) => {
-  await context.reply('\\*\\*Notice: nhitomi\'s Discord download command has been deprecated. You can use the web interface instead!')
+  await context.reply('**Notice**: nhitomi\'s Discord download command has been deprecated. You can use the web interface instead!\n---')
 
   return await runInternal(context, arg)
 }
