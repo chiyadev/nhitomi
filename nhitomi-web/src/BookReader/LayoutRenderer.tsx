a592e6a221247101ffb3662cfce972d7b7037736
phosphene47 <phosphene47@chiya.dev>
2020-05-18 02:08:34 +1000

    Fix missing closing bracket

--- nhitomi-web/src/BookReader/LayoutRenderer.tsx
+++ nhitomi-web/src/BookReader/LayoutRenderer.tsx
@@ -134,7 +134,7 @@ const Spinner = ({ style }: { style: CSSProperties }) => {
           position: 'relative',
           left: '50%',
           top: '50%',
-          transform: 'translate(-50%, -50%'
+          transform: 'translate(-50%, -50%)'
         }} />,
         [
           visible
