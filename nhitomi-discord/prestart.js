4142da3b9b317cb32cde5ae3136868328a51ca7a
phosphene47 <phosphene47@chiya.dev>
2020-05-17 21:45:52 +1000

    Move nhitomi-discord locales

--- nhitomi-discord/prestart.js
+++ nhitomi-discord/prestart.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const symlink = require('symlink-dir')
 
-symlink('../locales', 'build/locales')
+symlink('locales', 'build/locales')
 symlink('config', 'build/config')
