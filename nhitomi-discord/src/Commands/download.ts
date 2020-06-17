d098e9a30c39291b6f615e57cdac23c1709fb3eb
phosphene47 <phosphene47@chiya.dev>
2020-06-17 17:25:24 +1000

    Add link to web interface

--- nhitomi-discord/src/Commands/download.ts
+++ nhitomi-discord/src/Commands/download.ts
@@ -1,10 +1,15 @@
 import { CommandFunc } from '.'
 import { run as runInternal } from './get'
+import { Api } from '../api'
 
 // todo: remove this deprecation notice completely in August 2020
 
 export const run: CommandFunc = async (context, arg) => {
-  await context.reply('**Notice**: nhitomi\'s Discord download command has been deprecated. You can use the web interface instead!\n---')
+  await context.reply(`
+**Notice**: nhitomi\\'s Discord download command has been deprecated. You can use the web interface instead!
+>${Api.getWebLink('/')}>
+---
+`.trim())
 
   return await runInternal(context, arg)
 }
