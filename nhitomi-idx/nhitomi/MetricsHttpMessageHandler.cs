773457f1f14c0e78ffd96bf9e9f9e03ab5ba21d3
phosphene47 <phosphene47@chiya.dev>
2020-06-21 14:12:35 +1000

    Change http handler prefix to not conflict with kestrel metrics

--- nhitomi-idx/nhitomi/MetricsHttpMessageHandler.cs
+++ nhitomi-idx/nhitomi/MetricsHttpMessageHandler.cs
@@ -10,13 +10,15 @@ namespace nhitomi
     /// </summary>
     public class MetricsHttpMessageHandler : DelegatingHandler
     {
-        static readonly Histogram _time = Metrics.CreateHistogram("http_time_milliseconds", "Time spent on performing HTTP requests.", new HistogramConfiguration
+        // prefix should be "http_handler" because "http" usually relates to our kestrel server
+
+        static readonly Histogram _time = Metrics.CreateHistogram("http_handler_time_milliseconds", "Time spent on performing HTTP requests.", new HistogramConfiguration
         {
             Buckets    = HistogramEx.ExponentialBuckets(100, 10000, 10),
             LabelNames = new[] { "host", "method" }
         });
 
-        static readonly Counter _statuses = Metrics.CreateCounter("http_responses", "Number of HTTP responses.", new CounterConfiguration
+        static readonly Counter _statuses = Metrics.CreateCounter("http_handler_responses", "Number of HTTP responses.", new CounterConfiguration
         {
             LabelNames = new[] { "status" }
         });
