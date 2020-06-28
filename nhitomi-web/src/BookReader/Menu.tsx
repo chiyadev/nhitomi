3babc33731bf4a21ac982741d51cda84e0207749
phosphene47 <phosphene47@chiya.dev>
2020-06-28 10:25:14 +1000

    Add information panel to reader menu

--- nhitomi-web/src/BookReader/Menu.tsx
+++ nhitomi-web/src/BookReader/Menu.tsx
@@ -1,12 +1,15 @@
 import { useContext } from 'react'
 import { BookReaderContext } from '.'
 import React from 'react'
-import { Drawer } from 'antd'
+import { Drawer, Collapse, Descriptions } from 'antd'
 import { useShortcut } from '../shortcuts'
 import { LayoutContext } from '../LayoutContext'
+import { FormattedMessage } from 'react-intl'
+import { TimeDisplay } from '../TimeDisplay'
+import { BookTagList, TagDisplay } from '../Tags'
 
 export const Menu = () => {
-  const { book, menu, setMenu } = useContext(BookReaderContext)
+  const { book, content, menu, setMenu } = useContext(BookReaderContext)
   const { width } = useContext(LayoutContext)
 
   useShortcut('bookReaderMenuKey', () => setMenu(true))
@@ -19,9 +22,43 @@ export const Menu = () => {
       onClose={() => setMenu(false)}
       width={Math.min(600, width)}>
 
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <Collapse defaultActiveKey={['info']}>
+        <Collapse.Panel
+          key='info'
+          header={<FormattedMessage id='bookReader.menu.info.header' />}>
+
+          <Descriptions size='middle' column={2}>
+            <Descriptions.Item span={2} label='ID'>{book.id}/{content.id}</Descriptions.Item>
+
+            <Descriptions.Item label={<FormattedMessage id='bookReader.menu.info.uploadedTime' />}><TimeDisplay time={book.createdTime} /></Descriptions.Item>
+            <Descriptions.Item label={<FormattedMessage id='bookReader.menu.info.updatedTime' />}><TimeDisplay time={book.updatedTime} /></Descriptions.Item>
+
+            <Descriptions.Item label={<FormattedMessage id='bookReader.menu.info.category' />}><FormattedMessage id={`bookCategories.${book.category}`} /></Descriptions.Item>
+            <Descriptions.Item label={<FormattedMessage id='bookReader.menu.info.rating' />}><FormattedMessage id={`materialRatings.${book.rating}`} /></Descriptions.Item>
+
+            <Descriptions.Item label={<FormattedMessage id='bookReader.menu.info.pages' />}>{content.pageCount}</Descriptions.Item>
+            <Descriptions.Item label={<FormattedMessage id='bookReader.menu.info.notes' />}>{Object.values(content.notes).length}</Descriptions.Item>
+
+            <Descriptions.Item span={2} label={<FormattedMessage id='bookReader.menu.info.primaryName' />}>{book.primaryName}</Descriptions.Item>
+            <Descriptions.Item span={2} label={<FormattedMessage id='bookReader.menu.info.englishName' />}>{book.englishName}</Descriptions.Item>
+            <Descriptions.Item span={2} label={<FormattedMessage id='bookReader.menu.info.source' />}><a href={content.sourceUrl} target='_blank' rel='noopener noreferrer'>{content.sourceUrl}</a></Descriptions.Item>
+
+            <Descriptions.Item span={2} label={<FormattedMessage id='bookReader.menu.info.tags' />}>
+              <div>
+                {BookTagList.flatMap(type => book.tags[type]?.map(value => (
+                  <TagDisplay key={`${type}:${value}`} tag={type} value={value} />
+                )))}
+              </div>
+            </Descriptions.Item>
+          </Descriptions>
+        </Collapse.Panel>
+
+        <Collapse.Panel
+          key='scrapers'
+          header={<FormattedMessage id='bookReader.menu.keys.header' />}>
+
+        </Collapse.Panel>
+      </Collapse>
     </Drawer>
   )
 }
