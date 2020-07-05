1c586b0db745aa166145393526c30600d16500cb
phosphene47 <phosphene47@chiya.dev>
2020-07-05 20:39:21 +1000

    Add special collection indication

--- nhitomi-web/src/CollectionListing/BookGrid.tsx
+++ nhitomi-web/src/CollectionListing/BookGrid.tsx
@@ -1,12 +1,13 @@
-import React, { useContext } from 'react'
-import { List, Card, Typography, Empty } from 'antd'
+import React, { useContext, ReactElement, useState } from 'react'
+import { List, Card, Typography, Empty, Tooltip } from 'antd'
 import { ListGridType } from 'antd/lib/list'
 import { LayoutContext } from '../LayoutContext'
-import { Collection, Book } from '../Client'
+import { Collection, Book, User, SpecialCollection } from '../Client'
 import { ClientContext } from '../ClientContext'
 import { AsyncImage } from '../AsyncImage'
 import { CollectionContentLink } from '../CollectionContent'
 import { FormattedMessage } from 'react-intl'
+import { HeartFilled, EyeFilled } from '@ant-design/icons'
 
 const gridGutter = 6
 const gridLayout: ListGridType = {
@@ -18,7 +19,7 @@ const gridLayout: ListGridType = {
   xxl: 7
 }
 
-export const BookGrid = ({ items }: { items: { collection: Collection, cover?: Book }[] }) => {
+export const BookGrid = ({ user, items }: { user: User, items: { collection: Collection, cover?: Book }[] }) => {
   const { width, getBreakpoint } = useContext(LayoutContext)
 
   if (!items.length)
@@ -32,11 +33,16 @@ export const BookGrid = ({ items }: { items: { collection: Collection, cover?: B
       }}
       dataSource={items}
       rowKey={({ collection }) => collection.id}
-      renderItem={({ collection, cover }) => <Item collection={collection} cover={cover} />} />
+      renderItem={({ collection, cover }) => (
+        <Item
+          collection={collection}
+          cover={cover}
+          special={Object.entries(user.specialCollections?.book || {}).find(([, id]) => id === collection.id)?.[0] as SpecialCollection} />
+      )} />
   )
 }
 
-const Item = ({ collection, cover }: { collection: Collection, cover?: Book }) => {
+const Item = ({ collection, cover, special }: { collection: Collection, cover?: Book, special?: SpecialCollection }) => {
   const client = useContext(ClientContext)
   const coverContent = cover && (cover.contents.find(c => client.currentInfo.authenticated && c.language === client.currentInfo.user.language) || cover.contents[0])
 
@@ -56,7 +62,10 @@ const Item = ({ collection, cover }: { collection: Collection, cover?: Book }) =
               resize='fill'
               fluid
               preloadScale={0.5}
-              src={cover && coverContent ? (() => client.book.getBookImage({ id: cover.id, contentId: coverContent.id, index: -1 })) : undefined} />
+              src={cover && coverContent ? (() => client.book.getBookImage({ id: cover.id, contentId: coverContent.id, index: -1 })) : undefined}>
+
+              {special && <SpecialCollectionIcon type={special} />}
+            </AsyncImage>
           )}>
 
           <Card.Meta description={(
@@ -71,5 +80,45 @@ const Item = ({ collection, cover }: { collection: Collection, cover?: Book }) =
         </Card>
       </CollectionContentLink>
     </List.Item>
+  )
+}
+
+const SpecialCollectionIcon = ({ type }: { type: SpecialCollection }) => {
+  let icon: ReactElement
+
+  switch (type) {
+    case SpecialCollection.Favorites:
+      icon = <HeartFilled />
+      break
+
+    case SpecialCollection.Later:
+      icon = <EyeFilled />
+      break
+  }
+
+  const [hovered, setHovered] = useState(false)
+
+  icon = (
+    <span
+      children={icon}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)} />
+  )
+
+  return (
+    <div style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      opacity: hovered ? 1 : 0.8,
+      background: 'linear-gradient(to bottom right, black 0%, transparent 15%)',
+      color: 'white',
+      padding: 5,
+      transition: 'opacity 0.2s'
+    }}>
+      <Tooltip children={icon} title={<FormattedMessage id={`specialCollections.${type}`} />} />
+    </div>
   )
 }
