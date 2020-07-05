1c622692f0aef91f3b8be39ea559aa88bf5c75e1
phosphene47 <phosphene47@chiya.dev>
2020-07-06 03:13:28 +1000

    Add menu to add to favorites

--- nhitomi-web/src/BookReader/Menu.tsx
+++ nhitomi-web/src/BookReader/Menu.tsx
@@ -1,13 +1,15 @@
 import React, { useMemo, useContext } from 'react'
 import { Menu, Typography } from 'antd'
-import { Book, BookContent, BookTag } from '../Client'
-import { FileTextOutlined, ExpandAltOutlined, LinkOutlined, SearchOutlined } from '@ant-design/icons'
+import { Book, BookContent, BookTag, SpecialCollection, ObjectType, CollectionInsertPosition } from '../Client'
+import { FileTextOutlined, ExpandAltOutlined, LinkOutlined, SearchOutlined, HeartOutlined } from '@ant-design/icons'
 import { FormattedMessage } from 'react-intl'
 import { BookReaderLink } from '.'
 import { useCopyToClipboard } from 'react-use'
 import { NotificationContext } from '../NotificationContext'
 import { BookListingLink } from '../BookListing'
 import { SearchQuery } from '../BookListing/searchManager'
+import { ProgressContext } from '../Progress'
+import { ClientContext } from '../ClientContext'
 
 export const ReaderMenu = ({ book, content, setDetails }: {
   book: Book
@@ -15,7 +17,9 @@ export const ReaderMenu = ({ book, content, setDetails }: {
 
   setDetails: (details: boolean) => void
 }) => {
-  const { alert: { success } } = useContext(NotificationContext)
+  const client = useContext(ClientContext)
+  const { start, stop } = useContext(ProgressContext)
+  const { notification: { error }, alert: { success } } = useContext(NotificationContext)
   const [, setClipboard] = useCopyToClipboard()
 
   return useMemo(() => (
@@ -59,13 +63,41 @@ export const ReaderMenu = ({ book, content, setDetails }: {
       <Menu.Divider />
 
       <Menu.Item
+        icon={<HeartOutlined />}
+        onClick={async () => {
+          if (!client.currentInfo.authenticated) return
+
+          start()
+
+          try {
+            const collection = client.currentInfo.user.specialCollections?.book?.favorites || (await client.user.getUserSpecialCollection({ id: client.currentInfo.user.id, collection: SpecialCollection.Favorites, type: ObjectType.Book })).id
+            await client.collection.addCollectionItems({ id: collection, addCollectionItemsRequest: { items: [book.id], position: CollectionInsertPosition.Start } })
+
+            // todo: add link to fav collection here
+            // not possible at the moment due to missing contexts in notification provider
+            success(<FormattedMessage id='bookReader.menu.favorite.success' />)
+          }
+          catch (e) {
+            error(e)
+          }
+          finally {
+            stop()
+          }
+        }}>
+
+        <FormattedMessage id='bookReader.menu.favorite.title' />
+      </Menu.Item>
+
+      <Menu.Divider />
+
+      <Menu.Item
         icon={<FileTextOutlined />}
         onClick={() => setDetails(true)}>
 
         <FormattedMessage id='bookReader.menu.details' />
       </Menu.Item>
     </Menu>
-  ), [book, content, setClipboard, setDetails, success])
+  ), [client, book, content, setClipboard, setDetails, error, success, start, stop])
 }
 
 const OpenNewTabItem = ({ id, contentId, ...props }: { id: string, contentId: string, [key: string]: any }) => (
