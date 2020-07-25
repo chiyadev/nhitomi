4918e2be5714639e111955d8ac6312220a67ece4
phosphene47 <phosphene47@chiya.dev>
2020-07-25 23:46:05 +1000

    Fix hardcoded header background color

--- nhitomi-web/src/BookListing/AffixGradientPageHeader.tsx
+++ nhitomi-web/src/BookListing/AffixGradientPageHeader.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode, useState } from 'react'
 import { Affix } from 'antd'
+import { presetPrimaryColors } from '@ant-design/colors'
+
+const bg = presetPrimaryColors['@component-background']
 
 export const AffixGradientPageHeader = ({ children }: { children?: ReactNode }) => {
   const [affixed, setAffixed] = useState(false)
@@ -12,7 +15,7 @@ export const AffixGradientPageHeader = ({ children }: { children?: ReactNode }) 
 
       <Affix onChange={v => setAffixed(v || false)}>
         <div children={children} style={{
-          background: !affixed || hovered ? 'linear-gradient(black 0%, black 100%)' : 'linear-gradient(black 0%, transparent 100%)',
+          background: !affixed || hovered ? `linear-gradient(${bg} 0%, ${bg} 100%)` : `linear-gradient(${bg} 0%, transparent 100%)`,
           opacity: affixed && !hovered ? 0.8 : 1,
           transition: 'background 0.2s, opacity 0.2s'
         }} />
