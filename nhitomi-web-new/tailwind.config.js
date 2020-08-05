a699d8c77a76bac0b0a57489ee2a9dbf6333e6c1
phosphene47 <phosphene47@chiya.dev>
2020-08-05 19:21:51 +1000

    Make colors accessible from code

--- nhitomi-web-new/tailwind.config.js
+++ nhitomi-web-new/tailwind.config.js
@@ -1,7 +1,26 @@
 const palx = require('palx')
 const theme = require('tailwindcss/defaultTheme')
+const { writeFileSync } = require('fs')
 
-const colors = palx('#5da2d5')
+const colorSrc = palx('#5da2d5')
+const colors = Object.keys(colorSrc).reduce((hues, hue) => {
+  if (typeof colorSrc[hue] === 'object') {
+    hues[hue] = colorSrc[hue].reduce((lums, lum, i) => {
+      if (1 <= i && i <= 9) {
+        lums[i * 100] = lum
+      }
+
+      return lums
+    }, {})
+  }
+
+  return hues
+}, {
+  white: '#fcfcfc',
+  black: '#18171a'
+})
+
+writeFileSync('src/theme.json', JSON.stringify({ colors }))
 
 module.exports = {
   theme: {
@@ -9,22 +28,7 @@ module.exports = {
       sm: { max: '767px' },
       lg: { min: '768px' }
     },
-    colors: Object.keys(colors).reduce((hues, hue) => {
-      if (typeof colors[hue] === 'object') {
-        hues[hue] = colors[hue].reduce((lums, lum, i) => {
-          if (1 <= i && i <= 9) {
-            lums[i * 100] = lum
-          }
-
-          return lums
-        }, {})
-      }
-
-      return hues
-    }, {
-      white: '#fcfcfc',
-      black: '#18171a'
-    }),
+    colors,
     fontFamily: {
       sans: [
         '"Lexend Deca"',
