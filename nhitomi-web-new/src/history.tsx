fb922eb62d7d3d6b3348c3104196a5c958be34ae
phosphene47 <phosphene47@chiya.dev>
2020-08-07 20:22:45 +1000

    Use previous location when navigate unspecified

--- nhitomi-web-new/src/history.tsx
+++ nhitomi-web-new/src/history.tsx
@@ -1,23 +1,5 @@
 import { createBrowserHistory, History as Hisotry, Hash, Search, Pathname } from 'history'
 
-// https://stackoverflow.com/a/53307588/13160620
-let refreshed = false
-
-try {
-  const entry = performance.getEntriesByType('navigation')[0]
-
-  refreshed = entry instanceof PerformanceNavigationTiming && entry.type === 'reload'
-}
-catch {
-  try { refreshed = performance.navigation.type === 1 }
-  catch { /* ignored */ }
-}
-
-if (refreshed) {
-  // clear all stale states except scroll position
-  window.history.replaceState({ state: { scroll: window.history.state?.state?.scroll } }, document.title)
-}
-
 export const History: Hisotry<null | HistoryState> = createBrowserHistory() as any
 
 export type HistoryState = {
@@ -45,9 +27,27 @@ export function navigate(mode: NavigationMode, { path, search, hash, state }: { 
   // console.log('navigate', mode, path, search, hash, state)
 
   run({
-    pathname: path,
-    search,
-    hash,
+    pathname: path || current.pathname,
+    search: search || current.search,
+    hash: hash || current.hash,
     state: state || current.state
   })
+}
+
+// https://stackoverflow.com/a/53307588/13160620
+let refreshed = false
+
+try {
+  const entry = performance.getEntriesByType('navigation')[0]
+
+  refreshed = entry instanceof PerformanceNavigationTiming && entry.type === 'reload'
+}
+catch {
+  try { refreshed = performance.navigation.type === 1 }
+  catch { /* ignored */ }
+}
+
+if (refreshed) {
+  // clear all stale states except scroll position
+  navigate('replace', { state: s => ({ scroll: s.scroll }) })
 }
