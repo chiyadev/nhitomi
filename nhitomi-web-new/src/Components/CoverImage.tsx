1b3a289545a106a195df7a00019221ca46034b1c
phosphene47 <phosphene47@chiya.dev>
2020-08-09 01:55:21 +1000

    Handle errors and prolonged loads in cover image

--- nhitomi-web-new/src/Components/CoverImage.tsx
+++ nhitomi-web-new/src/Components/CoverImage.tsx
@@ -1,32 +1,74 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { useAsync } from 'react-use'
+import { cx, css } from 'emotion'
+import { Loading3QuartersOutlined, WarningTwoTone } from '@ant-design/icons'
+import { AbsoluteCenter } from './AbsoluteCenter'
+import { colors } from '../theme.json'
+import { Tooltip } from './Tooltip'
+import { FormattedMessage } from 'react-intl'
 
 export const CoverImage = ({ onLoad, className }: { onLoad: () => Promise<Blob> | Blob, className?: string }) => {
-  const { loading, error, value: loaded } = useAsync(async () => {
-    const blob = await onLoad()
+  const [prolongedLoad, setProlongedLoad] = useState(false) // if load is prolonged, show loading indicator
 
-    return URL.createObjectURL(blob)
+  const { loading, error, value: loaded } = useAsync(async () => {
+    const timer = window.setTimeout(() => setProlongedLoad(true), 2000)
+
+    try {
+      const blob = await onLoad()
+
+      return URL.createObjectURL(blob)
+    }
+    finally {
+      clearTimeout(timer)
+    }
   }, [])
 
   useLayoutEffect(() => () => { loaded && URL.revokeObjectURL(loaded) }, [loaded])
 
-  const style = useSpring(loaded
-    ? {
-      opacity: 1,
-      transform: 'scale(1)'
-    } : {
-      opacity: 0,
-      transform: 'scale(0.9)'
-    })
+  const imageStyle = useSpring({
+    opacity: loaded ? 1 : 0,
+    transform: loaded ? 'scale(1)' : 'scale(0.9)'
+  })
+
+  const loadingStyle = useSpring({
+    opacity: prolongedLoad && loading ? 1 : 0
+  })
+
+  const errorStyle = useSpring({
+    opacity: error ? 1 : 0
+  })
 
   return (
-    <animated.div
-      style={{
-        ...style,
-        backgroundImage: loaded ? `url(${loaded})` : undefined,
-        backgroundSize: 'cover'
-      }}
-      className={className} />
+    <div className={cx('relative', className)}>
+      {loaded && (
+        <animated.div
+          style={imageStyle}
+          className={cx('absolute top-0 left-0 w-full h-full', css`
+            background-image: ${loaded ? `url(${loaded})` : 'none'};
+            background-size: cover;
+          `)} />
+      )}
+
+      {prolongedLoad && loading && (
+        <animated.div style={loadingStyle} className={AbsoluteCenter}>
+          <Loading3QuartersOutlined className='animate-spin align-middle' />
+        </animated.div>
+      )}
+
+      {error && (
+        <animated.div style={errorStyle} className={AbsoluteCenter}>
+          <Tooltip
+            placement='bottom'
+            overlay={<>
+              <p><FormattedMessage id='components.coverImage.error' /></p>
+              <p><code>{error.message || <FormattedMessage id='components.coverImage.errorUnknown' />}</code></p>
+            </>}>
+
+            <WarningTwoTone className='align-middle' twoToneColor={colors.red[500]} />
+          </Tooltip>
+        </animated.div>
+      )}
+    </div>
   )
 }
