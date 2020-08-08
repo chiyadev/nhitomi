a7cf7ac3ba2b6a539dcf8976e4e639c59f010f29
phosphene47 <phosphene47@chiya.dev>
2020-08-08 18:12:43 +1000

    Use spring in icon button

--- nhitomi-web-new/src/Components/RoundIconButton.tsx
+++ nhitomi-web-new/src/Components/RoundIconButton.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, useState } from 'react'
 import { cx } from 'emotion'
+import { useSpring, animated } from 'react-spring'
 
 export const RoundIconButton = ({ className, backColor, children }: {
   className?: string
@@ -7,16 +8,19 @@ export const RoundIconButton = ({ className, backColor, children }: {
   children?: ReactNode
 }) => {
   const [hover, setHover] = useState(false)
+  const style = useSpring({ opacity: hover ? 0.25 : 0 })
 
   return (
     <div
-      className={cx('w-10 h-10 rounded-full overflow-hidden', className)}
+      className={cx('w-10 h-10 rounded-full overflow-hidden relative', className)}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}>
 
-      <div className={cx('w-full h-full flex items-center justify-center transition duration-75', backColor || 'bg-white', hover ? 'bg-opacity-25' : 'bg-opacity-0')}>
+      <div className='w-full h-full flex items-center justify-center'>
         {children}
       </div>
+
+      <animated.span style={style} className={cx('absolute top-0 w-full h-full', backColor || 'bg-white')} />
     </div>
   )
 }
