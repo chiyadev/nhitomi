b3d7f92661b77715b7998c74e23965f516b7cb1e
phosphene47 <phosphene47@chiya.dev>
2020-08-09 03:09:12 +1000

    Install react-modal

--- nhitomi-web-new/src/index.tsx
+++ nhitomi-web-new/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './App'
+import Modal from 'react-modal'
 
 import './theme.css'
 import './theme-special'
 import './index.css'
+
+Modal.setAppElement('#root')
 
 render(
   <React.StrictMode>
