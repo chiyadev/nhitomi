c97135791f7221d79c111c493095d8fd517a3fc2
phosphene47 <phosphene47@chiya.dev>
2020-08-08 17:34:16 +1000

    Implement custom tooltip appearance

--- nhitomi-web-new/src/Components/Tooltip.tsx
+++ nhitomi-web-new/src/Components/Tooltip.tsx
@@ -1,25 +1,48 @@
-import React, { ReactNode, ComponentProps } from 'react'
+import React, { ReactNode, ComponentProps, useState } from 'react'
 import Tippy from '@tippyjs/react'
 import { cx } from 'emotion'
+import { animated, useSpring } from 'react-spring'
 
-import 'tippy.js/dist/tippy.css'
-
-export const Tooltip = ({ className, overlay, children, animation = 'fade', hideOnClick = false, ignoreAttributes = true, touch = false, duration = 200, ...props }: {
+export const Tooltip = ({ className, overlay, children, hideOnClick = false, ignoreAttributes = true, touch = false, duration = 200, placement = 'auto', onShow, onHide, ...props }: {
   overlay?: ReactNode
   children?: ReactNode
-} & Omit<ComponentProps<typeof Tippy>, 'content' | 'children'>) => {
+} & Omit<ComponentProps<typeof Tippy>, 'content' | 'children' | 'animation' | 'arrow'>) => {
+  const [visible, setVisible] = useState(false)
+
+  const style = useSpring({
+    opacity: visible ? 1 : 0,
+    marginTop: placement.indexOf('bottom') === -1 ? 0 : visible ? 0 : -5,
+    marginRight: placement.indexOf('left') === -1 ? 0 : visible ? 0 : -5,
+    marginBottom: placement.indexOf('top') === -1 ? 0 : visible ? 0 : -5,
+    marginLeft: placement.indexOf('right') === -1 ? 0 : visible ? 0 : -5
+  })
+
   return (
     <Tippy
-      content={(
-        <span
-          className={cx(className, 'rounded overflow-hidden')}
-          children={overlay} />
+      render={props => (
+        <animated.span
+          {...props}
+          style={style}
+          className={cx(className, 'rounded overflow-hidden text-xs px-2 py-1 bg-gray-900 text-white')}>
+
+          {overlay}
+        </animated.span>
       )}
-      animation={animation}
       hideOnClick={hideOnClick}
       ignoreAttributes={ignoreAttributes}
       touch={touch}
       duration={duration}
+      placement={placement}
+
+      onShow={x => {
+        setVisible(true)
+        return onShow?.(x)
+      }}
+      onHide={x => {
+        setVisible(false)
+        return onHide?.(x)
+      }}
+
       {...props}>
 
       <span className='block'>{children}</span>
