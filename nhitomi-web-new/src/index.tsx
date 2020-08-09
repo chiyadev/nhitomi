e8a2395f6d73ca8e4d06befcc4a5e6a4d3b6decb
phosphene47 <phosphene47@chiya.dev>
2020-08-09 12:44:33 +1000

    Remove react-modal

--- nhitomi-web-new/src/index.tsx
+++ nhitomi-web-new/src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './App'
-import Modal from 'react-modal'
 
 import './theme.css'
 import './theme-special'
 import './index.css'
-
-Modal.setAppElement('#root')
 
 render(
   <React.StrictMode>
