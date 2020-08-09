406ef852f3fc81ef9566cccd1ff350369f53bfae
phosphene47 <phosphene47@chiya.dev>
2020-08-10 01:11:53 +1000

    Respect animation setting for progress

--- nhitomi-web-new/src/ProgressManager.tsx
+++ nhitomi-web-new/src/ProgressManager.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, ReactNode, useRef, useLayoutEffect, useMemo, useContext } from 'react'
 import nprogress from 'nprogress'
+import { useConfig } from './ConfigManager'
 
 import './Progress.css'
 
@@ -14,8 +15,17 @@ export function useProgress() {
 
 export const ProgressManager = ({ children }: { children?: ReactNode }) => {
   const count = useRef(0)
+  const [mode] = useConfig('animation')
 
   useLayoutEffect(() => {
+    let easing: string
+
+    switch (mode) {
+      case 'normal': easing = 'ease'; break
+      case 'faster': easing = 'cubic-bezier(0, 1, 0, 1)'; break
+      case 'none': easing = 'steps-start'; break
+    }
+
     nprogress.configure({
       template: `
         <div class="bar" role="bar">
@@ -24,9 +34,10 @@ export const ProgressManager = ({ children }: { children?: ReactNode }) => {
         <div class="spinner" role="spinner">
           <div class="spinner-icon"></div>
         </div>
-      `
+      `,
+      easing
     })
-  }, [])
+  }, [mode])
 
   // timeout prevents flickering when fetching multiple resources in a short time
   const done = useRef<number>()
