f8ae07ff77f3af4c7ae66834eec92864ea227b0b
phosphene47 <phosphene47@chiya.dev>
2020-08-09 21:32:39 +1000

    Add hex converter utility

--- nhitomi-web-new/src/index.tsx
+++ nhitomi-web-new/src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { App } from './App'
 
 import './theme.css'
-import './theme-special'
+import './theme'
 import './index.css'
 
 render(
---
+++ nhitomi-web-new/src/theme.ts
@@ -0,0 +1,36 @@
+export { }
+
+function css(s: TemplateStringsArray) {
+  const style = document.createElement('style')
+  style.type = 'text/css'
+  style.innerText = s.toString()
+
+  document.head.appendChild(style)
+}
+
+// add beautiful frosty backgrounds for all browsers that support it! *looks with disgust at firefox*
+if (CSS.supports('backdrop-filter', 'blur(0)')) {
+  css`
+    .bg-blur {
+      backdrop-filter: blur(1em);
+      --bg-opacity: 75%;
+    }
+  `
+}
+
+/** Converts a hex color to CSS rgba(...) format. */
+export function convertHex(hex: string, alpha?: number) {
+  hex = hex.startsWith('#') ? hex.substring(1) : hex
+
+  if (hex.length === 3)
+    hex = hex.split('').flatMap(x => [x, x]).join('')
+
+  const r = parseInt(hex.slice(0, 2), 16)
+  const g = parseInt(hex.slice(2, 4), 16)
+  const b = parseInt(hex.slice(4, 6), 16)
+
+  if (typeof alpha === 'undefined')
+    return `rgba(${r}, ${g}, ${b})`
+  else
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`
+}
