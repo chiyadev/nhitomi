b8f1959283b9eec3a472ec36645040ea9ec89a06
phosphene47 <phosphene47@chiya.dev>
2020-08-12 01:24:58 +1000

    Implement dropdown control using tooltip

---
+++ nhitomi-web-new/src/Components/Dropdown.tsx
@@ -0,0 +1,57 @@
+import React, { ComponentProps, ReactNode, useState } from 'react'
+import { Tooltip } from './Tooltip'
+import { useSpring, animated } from 'react-spring'
+import { convertHex } from '../theme'
+import { cx } from 'emotion'
+import { colors } from '../theme.json'
+
+export const Dropdown = ({ interactive = true, placement = 'bottom-start', ...props }: Omit<ComponentProps<typeof Tooltip>, 'padding'>) => {
+  return (
+    <Tooltip
+      hideOnClick
+      interactive={interactive}
+      placement={placement}
+      padding={false}
+
+      {...props} />
+  )
+}
+
+export const DropdownItem = ({ children, className }: { children?: ReactNode, className?: string }) => {
+  const [hover, setHover] = useState(false)
+
+  const style = useSpring({
+    backgroundColor: convertHex('#fff', hover ? 0.1 : 0)
+  })
+
+  return (
+    <animated.div
+      className={cx('cursor-pointer px-2 py-1', className)}
+      style={style}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}>
+
+      {children}
+    </animated.div>
+  )
+}
+
+export const DropdownGroup = ({ name, children, className }: { name?: ReactNode, children?: ReactNode, className?: string }) => {
+  const [hover, setHover] = useState(false)
+
+  const style = useSpring({
+    color: hover ? colors.gray[500] : colors.gray[800]
+  })
+
+  return (
+    <div className={cx('pl-2', className)}>
+      <animated.div style={style} className='cursor-default py-1'>{name}</animated.div>
+
+      <div
+        className='rounded-l overflow-hidden'
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        children={children} />
+    </div>
+  )
+}
