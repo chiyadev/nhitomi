463ef5320c12157002e617292f4cf52544d08dd5
phosphene47 <phosphene47@chiya.dev>
2020-08-11 21:49:11 +1000

    Provide breakpoint information in layout context

--- nhitomi-web-new/src/LayoutManager.tsx
+++ nhitomi-web-new/src/LayoutManager.tsx
@@ -2,11 +2,54 @@ import React, { ReactNode, createContext, useMemo, useContext } from 'react'
 import { useWindowSize } from 'react-use'
 import { StripWidth } from './Sidebar/Strip'
 
-/** Represents the width after which the device will be considered large. */
-export const Breakpoint = 768
+export type ScreenType = 'sm' | 'lg'
+
+/** Width after which the device will be considered large. */
+export const ScreenBreakpoint = 768
 
 export const SmallBreakpoints = [320, 480, 640]
 export const LargeBreakpoints = [640, 768, 1024, 1280].map(n => n - StripWidth)
+
+/** Layout information context. */
+const LayoutContext = createContext<{
+  width: number
+  height: number
+  sidebar: number
+  screen: ScreenType
+  breakpoint?: number
+}>(undefined as any)
+
+export function useLayout() {
+  return useContext(LayoutContext)
+}
+
+export const LayoutManager = ({ children }: { children?: ReactNode }) => {
+  const { width: windowWidth, height } = useWindowSize()
+
+  let screen: ScreenType = 'sm'
+  let width = windowWidth
+  let sidebar = 0
+  let breakpoint = getBreakpoint(SmallBreakpoints, width)
+
+  if (width >= ScreenBreakpoint) {
+    screen = 'lg'
+    sidebar = StripWidth
+    width -= sidebar // on large screens there is a sidebar
+    breakpoint = getBreakpoint(LargeBreakpoints, width)
+  }
+
+  return (
+    <LayoutContext.Provider
+      value={useMemo(() => ({
+        width,
+        height,
+        sidebar,
+        screen,
+        breakpoint
+      }), [width, height, sidebar, screen, breakpoint])}
+      children={children} />
+  )
+}
 
 export function getBreakpoint(breakpoints: number[], value: number) {
   let breakpoint: number | undefined
@@ -19,34 +62,4 @@ export function getBreakpoint(breakpoints: number[], value: number) {
   }
 
   return breakpoint
-}
-
-/** Layout information context. */
-const LayoutContext = createContext<{
-  width: number
-  height: number
-  sidebar: number
-  screen: 'sm' | 'lg'
-}>(undefined as any)
-
-export function useLayout() {
-  return useContext(LayoutContext)
-}
-
-export const LayoutManager = ({ children }: { children?: ReactNode }) => {
-  const { width, height } = useWindowSize()
-
-  return (
-    <LayoutContext.Provider
-      value={useMemo(() => ({
-        width: width >= Breakpoint ? width - StripWidth : width,
-        height,
-        sidebar: width >= Breakpoint ? StripWidth : 0,
-        screen: width >= Breakpoint ? 'lg' : 'sm'
-      }), [
-        width,
-        height
-      ])}
-      children={children} />
-  )
 }
