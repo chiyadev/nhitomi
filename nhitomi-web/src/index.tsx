2ad967582dce4db5db7232594a93ae4272be188e
phosphene47 <phosphene47@chiya.dev>
2020-08-12 17:06:47 +1000

    Delete legacy nhitomi website

--- nhitomi-web/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
-
-import './localStorage'
-import './logging'
-
-ReactDOM.render(<App />, document.getElementById('root'))
--- nhitomi-web/src/safariTest.ts
+++
@@ -1,7 +0,0 @@
-// this is stupid but we need it
-
-// https://github.com/necolas/react-native-web/issues/1369#issuecomment-502476921
-// https://hackernoon.com/onresize-event-broken-in-mobile-safari-d8469027bf4d
-
-export const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-export const safariResizeDelay = 500
