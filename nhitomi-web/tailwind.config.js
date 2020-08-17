b8a8f53d0c7c72e5ae33055e6970c33752d27933
phosphene47 <phosphene47@chiya.dev>
2020-08-17 19:14:55 +1000

    Use open color scheme

--- nhitomi-web/tailwind.config.js
+++ nhitomi-web/tailwind.config.js
@@ -1,26 +1,5 @@
-const palx = require('palx')
 const theme = require('tailwindcss/defaultTheme')
-const { writeFileSync } = require('fs')
-
-const colorSrc = palx('#5da2d5')
-const colors = Object.keys(colorSrc).reduce((hues, hue) => {
-  if (typeof colorSrc[hue] === 'object') {
-    hues[hue] = colorSrc[hue].reduce((lums, lum, i) => {
-      if (1 <= i && i <= 9) {
-        lums[i * 100] = lum
-      }
-
-      return lums
-    }, {})
-  }
-
-  return hues
-}, {
-  white: '#fcfcfc',
-  black: '#18171a'
-})
-
-writeFileSync('src/theme.json', JSON.stringify({ colors }))
+const { colors } = require('./src/theme.json')
 
 module.exports = {
   theme: {
