39b444c5c5d61a57e8136fa84827579445eab023
phosphene47 <phosphene47@chiya.dev>
2020-08-19 15:28:34 +1000

    Disable sidebar if client not authenticated

--- nhitomi-web/src/Sidebar/Strip.tsx
+++ nhitomi-web/src/Sidebar/Strip.tsx
@@ -9,68 +9,80 @@ import { SettingsLink } from '../Settings'
 import { Route, Switch } from 'react-router-dom'
 import { useSpring, animated } from 'react-spring'
 import { SelfCollectionListingLink } from '../CollectionListing'
+import { useClientInfo } from '../ClientManager'
+import { Disableable } from '../Components/Disableable'
 
 export const StripWidth = 64
 
 export const Strip = () => {
+  const { info } = useClientInfo()
+
   const style = useSpring({
     from: { opacity: 0, marginLeft: -5 },
     to: { opacity: 1, marginLeft: 0 }
   })
 
   return (
-    <animated.div
-      style={style}
-      className={cx('fixed top-0 left-0 bottom-0 z-10 text-white py-4 flex flex-col items-center select-none', css`width: ${StripWidth}px;`)}>
+    <Disableable disabled={!info.authenticated}>
+      <animated.div
+        style={style}
+        className={cx('fixed top-0 left-0 bottom-0 z-10 text-white py-4 flex flex-col items-center select-none', css`width: ${StripWidth}px;`)}>
 
-      <Tooltip overlay={<span><FormattedMessage id='pages.home.title' /> <HeartOutlined /></span>} placement='right' className='mb-4'>
-        <BookListingLink>
-          <img alt='logo' className='w-10 h-10 pointer-events-none' src='/logo-40x40.png' />
-        </BookListingLink>
-      </Tooltip>
+        <Tooltip overlay={<span><FormattedMessage id='pages.home.title' /> <HeartOutlined /></span>} placement='right' className='mb-4'>
+          <BookListingLink>
+            <img alt='logo' className='w-10 h-10 pointer-events-none' src='/logo-40x40.png' />
+          </BookListingLink>
+        </Tooltip>
 
-      <Tooltip overlay={<FormattedMessage id='pages.bookListing.title' />} placement='right'>
-        <BookListingLink>
-          <RoundIconButton>
-            <Switch>
-              <Route path='/books'><ReadFilled /></Route>
-              <Route><BookOutlined /></Route>
-            </Switch>
-          </RoundIconButton>
-        </BookListingLink>
-      </Tooltip>
+        <Buttons />
+      </animated.div>
+    </Disableable>
+  )
+}
 
-      <Tooltip overlay={<FormattedMessage id='pages.collectionListing.title' />} placement='right'>
-        <SelfCollectionListingLink>
-          <RoundIconButton>
-            <Switch>
-              <Route path='/collections'><FolderOpenFilled /></Route>
-              <Route path='/users/:id/collections'><FolderOpenFilled /></Route>
-              <Route><FolderOutlined /></Route>
-            </Switch>
-          </RoundIconButton>
-        </SelfCollectionListingLink>
-      </Tooltip>
-
-      <Tooltip overlay={<FormattedMessage id='pages.settings.title' />} placement='right'>
-        <SettingsLink>
-          <RoundIconButton>
-            <Switch>
-              <Route path='/settings'><SettingFilled /></Route>
-              <Route><SettingOutlined /></Route>
-            </Switch>
-          </RoundIconButton>
-        </SettingsLink>
-      </Tooltip>
-
-      <Tooltip overlay={<FormattedMessage id='pages.about.title' />} placement='right'>
+const Buttons = () => {
+  return <>
+    <Tooltip overlay={<FormattedMessage id='pages.bookListing.title' />} placement='right'>
+      <BookListingLink>
         <RoundIconButton>
           <Switch>
-            <Route path='/about'><InfoCircleFilled /></Route>
-            <Route><InfoCircleOutlined /></Route>
+            <Route path='/books'><ReadFilled /></Route>
+            <Route><BookOutlined /></Route>
           </Switch>
         </RoundIconButton>
-      </Tooltip>
-    </animated.div>
-  )
+      </BookListingLink>
+    </Tooltip>
+
+    <Tooltip overlay={<FormattedMessage id='pages.collectionListing.title' />} placement='right'>
+      <SelfCollectionListingLink>
+        <RoundIconButton>
+          <Switch>
+            <Route path='/collections'><FolderOpenFilled /></Route>
+            <Route path='/users/:id/collections'><FolderOpenFilled /></Route>
+            <Route><FolderOutlined /></Route>
+          </Switch>
+        </RoundIconButton>
+      </SelfCollectionListingLink>
+    </Tooltip>
+
+    <Tooltip overlay={<FormattedMessage id='pages.settings.title' />} placement='right'>
+      <SettingsLink>
+        <RoundIconButton>
+          <Switch>
+            <Route path='/settings'><SettingFilled /></Route>
+            <Route><SettingOutlined /></Route>
+          </Switch>
+        </RoundIconButton>
+      </SettingsLink>
+    </Tooltip>
+
+    <Tooltip overlay={<FormattedMessage id='pages.about.title' />} placement='right'>
+      <RoundIconButton>
+        <Switch>
+          <Route path='/about'><InfoCircleFilled /></Route>
+          <Route><InfoCircleOutlined /></Route>
+        </Switch>
+      </RoundIconButton>
+    </Tooltip>
+  </>
 }
