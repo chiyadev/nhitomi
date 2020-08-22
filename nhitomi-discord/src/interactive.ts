0608cd30702872988fd223d0a61f49e974b407fb
phosphene47 <phosphene47@chiya.dev>
2020-08-22 13:28:44 +1000

    Don't delete command message in interactive

--- nhitomi-discord/src/interactive.ts
+++ nhitomi-discord/src/interactive.ts
@@ -211,9 +211,6 @@ export abstract class InteractiveMessage {
       for (const input of this.ownedInputs)
         input.reject()
 
-      try { if (!expiring && this.context && this.context.refCount <= 1 && this.context.message.deletable) await this.context.message.delete() }
-      catch { /* ignored */ }
-
       if (this.context) {
         delete interactives[this.context.message.id]
 
