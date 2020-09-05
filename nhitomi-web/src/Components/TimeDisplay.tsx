a30d73162f67990e34596f7a2453dc5d42323096
phosphene47 <phosphene47@chiya.dev>
2020-09-06 00:23:06 +1000

    Make time color different

--- nhitomi-web/src/Components/TimeDisplay.tsx
+++ nhitomi-web/src/Components/TimeDisplay.tsx
@@ -8,7 +8,11 @@ export const TimeDisplay = ({ value, className }: { value: Date, className?: str
     <Tooltip
       className={cx('inline-flex', className)}
       placement='top'
-      overlay={value.toUTCString()}>
+      overlay={<>
+        {value.toDateString()}
+        {' '}
+        <span className='text-blue font-bold'>{value.toTimeString()}</span>
+      </>}>
 
       <span className='cursor-default'>
         <FormattedDate value={value} />
