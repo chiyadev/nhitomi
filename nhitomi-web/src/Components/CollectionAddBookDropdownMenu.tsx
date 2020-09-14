d61a5fef79ef6a7b9ef2b845149437ab58347b99
phosphene47 <phosphene47@chiya.dev>
2020-09-14 20:46:32 +1000

    Allow creating collection from dropdown menu

--- nhitomi-web/src/Components/CollectionAddBookDropdownMenu.tsx
+++ nhitomi-web/src/Components/CollectionAddBookDropdownMenu.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { useClient, useClientInfo } from '../ClientManager'
 import { useNotify, useAlert } from '../NotificationManager'
 import { useState } from 'react'
-import { Collection, CollectionInsertPosition } from 'nhitomi-api'
+import { Collection, CollectionInsertPosition, ObjectType } from 'nhitomi-api'
 import { useAsync } from 'react-use'
-import { Loading3QuartersOutlined } from '@ant-design/icons'
+import { Loading3QuartersOutlined, PlusOutlined } from '@ant-design/icons'
 import { FormattedMessage } from 'react-intl'
-import { DropdownItem } from './Dropdown'
+import { DropdownItem, DropdownDivider } from './Dropdown'
 import { Disableable } from './Disableable'
 import { useProgress } from '../ProgressManager'
 import { CollectionContentLink } from '../CollectionContent'
 import { usePageState } from '../state'
+import { useLocalized } from '../LocaleManager'
+import { useDynamicPrefetch } from '../Prefetch'
+import { useCollectionEditPrefetch } from '../CollectionListing/Edit'
 
 type BasicBook = {
   id: string
@@ -42,9 +45,17 @@ export const CollectionAddBookDropdownMenu = ({ book }: { book: BasicBook }) => 
 
   return <>
     {collections
-      ? collections.map(collection => (
-        <CollectionAddItem book={book} collection={collection} />
-      ))
+      ? <>
+        {collections.map(collection => (
+          <Add book={book} collection={collection} />
+        ))}
+
+        {collections.length && (
+          <DropdownDivider />
+        )}
+
+        <Create book={book} />
+      </>
       : (
         <Disableable disabled>
           <DropdownItem>
@@ -55,7 +66,7 @@ export const CollectionAddBookDropdownMenu = ({ book }: { book: BasicBook }) => 
   </>
 }
 
-const CollectionAddItem = ({ book, collection }: { book: BasicBook, collection: Collection }) => {
+const Add = ({ book, collection }: { book: BasicBook, collection: Collection }) => {
   const client = useClient()
   const { begin, end } = useProgress()
   const { alert } = useAlert()
@@ -100,6 +111,61 @@ const CollectionAddItem = ({ book, collection }: { book: BasicBook, collection: 
 
         {collection.name}
       </DropdownItem>
+    </Disableable>
+  )
+}
+
+const Create = ({ book }: { book: BasicBook }) => {
+  const client = useClient()
+  const { begin, end } = useProgress()
+  const { notifyError } = useNotify()
+  const { alert } = useAlert()
+  const [loading, setLoading] = useState(false)
+  const [prefetchNode, navigate] = useDynamicPrefetch(useCollectionEditPrefetch)
+
+  const dummyName = useLocalized('components.collections.created.dummyName')
+
+  return (
+    <Disableable disabled={loading}>
+      <DropdownItem icon={<PlusOutlined />} onClick={async () => {
+        begin()
+        setLoading(true)
+
+        try {
+          let collection = await client.collection.createCollection({
+            createCollectionRequest: {
+              type: ObjectType.Book,
+              collection: {
+                name: dummyName
+              }
+            }
+          })
+
+          collection = await client.collection.addCollectionItems({
+            id: collection.id,
+            addCollectionItemsRequest: {
+              items: [book.id],
+              position: CollectionInsertPosition.Start
+            }
+          })
+
+          await navigate({ id: collection.id })
+
+          alert(<FormattedMessage id='components.collections.created.success' />, 'success')
+        }
+        catch (e) {
+          notifyError(e)
+        }
+        finally {
+          end()
+          setLoading(false)
+        }
+      }}>
+
+        <FormattedMessage id='components.collections.created.name' />
+      </DropdownItem>
+
+      {prefetchNode}
     </Disableable>
   )
 }
