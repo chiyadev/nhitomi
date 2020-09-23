2ee27adb82d67bd2b1697eac37783a1e8e660be8
phosphene47 <phosphene47@chiya.dev>
2020-09-23 18:14:05 +1000

    Enable tailwind breaking changes

--- nhitomi-web/tailwind.config.js
+++ nhitomi-web/tailwind.config.js
@@ -1,16 +1,16 @@
-const theme = require('tailwindcss/defaultTheme')
-const { colors } = require('./src/theme.json')
+const theme = require("tailwindcss/defaultTheme");
+const { colors } = require("./src/theme.json");
 
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   theme: {
     screens: false,
     colors,
     fontFamily: {
-      sans: [
-        '"Lexend Deca"',
-        '"M PLUS Rounded 1c"',
-        ...theme.fontFamily.sans
-      ]
-    }
-  }
-}
+      sans: ['"Lexend Deca"', '"M PLUS Rounded 1c"', ...theme.fontFamily.sans],
+    },
+  },
+};
