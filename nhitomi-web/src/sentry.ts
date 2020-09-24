85fa2dd09e3a970d9457a41163b22bea2ef2729c
phosphene47 <phosphene47@chiya.dev>
2020-09-24 16:23:21 +1000

    Set sentry release

--- nhitomi-web/src/sentry.ts
+++ nhitomi-web/src/sentry.ts
@@ -3,6 +3,8 @@ import { Integrations } from "@sentry/tracing";
 
 init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
+  release: `nhitomi@${process.env.REACT_APP_VERSION || "Latest"}`,
+  environment: process.env.NODE_ENV,
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 0.01,
 });
